a3167b487332a0232b22d50c3c3401b657cb5dfb
Liv Vitale <Liv.Vitale@oregonstate.edu>
2018-09-10 16:31:18 -0700

    Configured ESLint, Prettier

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-console */
+
 const app = require('express')();
+
 const PORT = process.env.port || 4000;
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
