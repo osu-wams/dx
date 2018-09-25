04c181e841025a2d286e3c7c30dc9ac8eca66c9f
Liv Vitale <Liv.Vitale@oregonstate.edu>
2018-09-25 11:58:53 -0700

    Added events API routes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 /* eslint-disable no-console */
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const logger = require('./logger');
 
+// App Configuration
 const app = express();
+app.use(bodyParser.json());
 
 const PORT = process.env.port || 4000;
 
+// Import API Routes
+const router = require('./api')(app);
+
+// Start Server
 app.listen(PORT, () => logger.info(`Server listening on port ${PORT}`));
