eb1d40a8d4238a677988930f0bfd44bcd321abe9
Liv Vitale <Liv.Vitale@oregonstate.edu>
2018-10-26 10:16:29 -0700

    Initial navbar mockup

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { Component, ReactDOM } from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faUser } from '@fortawesome/free-solid-svg-icons';
+import { Dialog as ReachDialog, DialogOverlay, DialogContent } from '@reach/dialog';
+import '@reach/dialog/styles.css';
+
+const Dialog = styled(ReachDialog)`
+  left: 0;
+  top: 0;
+  height: 100%;
+  margin: 0 !important;
+`;
 
 const NavbarWrapper = styled.header`
   height: 64px;
@@ -27,12 +36,53 @@ export const NavbarButton = styled.button`
   }
 `;
 
-const Navbar = () => (
-  <NavbarWrapper>
-    <NavbarButton>
-      <FontAwesomeIcon icon={faBars} size="2x" />
-    </NavbarButton>
-  </NavbarWrapper>
-);
+class Navbar extends Component {
+  state = {
+    showMenu: false
+  };
+
+  render() {
+    return (
+      <div style={{ marginBottom: '8px' }}>
+        <NavbarWrapper>
+          <NavbarButton style={{ marginLeft: 'auto' }}>
+            <FontAwesomeIcon icon={faUser} size="2x" />
+          </NavbarButton>
+        </NavbarWrapper>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            height: '40px',
+            overflow: 'hidden',
+            marginTop: '16px'
+          }}
+        >
+          <SwipeNavItem>Dashboard</SwipeNavItem>
+          <SwipeNavItem>Academics</SwipeNavItem>
+          <SwipeNavItem>Finances</SwipeNavItem>
+          <SwipeNavItem>Services</SwipeNavItem>
+          <SwipeNavItem>Events</SwipeNavItem>
+        </div>
+      </div>
+    );
+  }
+}
+
+const SwipeNavItem = styled.div`
+  text-transform: uppercase;
+  font-family: Stratum2;
+  font-weight: lighter;
+  margin: 0 4px;
+  padding: 0 4px;
+  height: 40px;
+  line-height: 44px;
+  :first-child {
+    margin-left: 16px;
+    border-bottom: 2px solid ${({ theme }) => theme.primary.bg};
+    color: ${({ theme }) => theme.primary.bg};
+    font-weight: bold;
+  }
+`;
 
 export default Navbar;
