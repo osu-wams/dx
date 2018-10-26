dbfa3514c90ca4367e9163d45d8e45f80f348f79
Liv Vitale <Liv.Vitale@oregonstate.edu>
2018-10-26 10:16:08 -0700

    Changed card component usage in app.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@
 
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import Card, { CardHeader, CardContent } from './components/layout/Card';
+import { faBookReader } from '@fortawesome/free-solid-svg-icons';
+import Card from './components/layout/Card';
 import theme from './theme';
-import GlobalStyles from './GlobalStyles';
 import Navbar from './components/layout/Navbar';
 import {
   Table,
@@ -14,62 +14,56 @@ import {
   TableHeader,
   TableHeaderCell
 } from './components/layout/Table';
-import Button from './components/layout/Button';
+import Badge from './components/layout/Badge';
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <div>
-      <GlobalStyles />
       <Navbar />
       <div style={{ padding: `${theme.spacing.unit * 2}px` }}>
-        <Card>
-          <CardHeader variant="primary">Header</CardHeader>
-          <CardContent>
-            <Table>
-              <TableHeader>
-                <TableHeaderCell>1</TableHeaderCell>
-                <TableHeaderCell>1</TableHeaderCell>
-                <TableHeaderCell>1</TableHeaderCell>
-              </TableHeader>
-              <TableBody>
-                <TableRow>
-                  <TableCell>1</TableCell>
-                  <TableCell>2</TableCell>
-                  <TableCell>3</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>1</TableCell>
-                  <TableCell>2</TableCell>
-                  <TableCell>3</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>1</TableCell>
-                  <TableCell>2</TableCell>
-                  <TableCell>3</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>1</TableCell>
-                  <TableCell>2</TableCell>
-                  <TableCell>3</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>1</TableCell>
-                  <TableCell>2</TableCell>
-                  <TableCell>3</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-            <Button variant="primary" outline size="small">
-              Button!
-            </Button>
-            <Button variant="academic" size="large">
-              Testo Large
-            </Button>
-            <Button variant="academic" size="normal">
-              Testo Normal
-            </Button>
-            <Button>Default</Button>
-          </CardContent>
+        <Card
+          title="Assignments"
+          headerIcon={faBookReader}
+          subtitle={
+            <Badge inline badgeContent={3}>
+              Due Soon
+            </Badge>
+          }
+        >
+          <Table>
+            <TableHeader>
+              <TableHeaderCell>1</TableHeaderCell>
+              <TableHeaderCell>1</TableHeaderCell>
+              <TableHeaderCell>1</TableHeaderCell>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell>1</TableCell>
+                <TableCell>2</TableCell>
+                <TableCell>3</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>1</TableCell>
+                <TableCell>2</TableCell>
+                <TableCell>3</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>1</TableCell>
+                <TableCell>2</TableCell>
+                <TableCell>3</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>1</TableCell>
+                <TableCell>2</TableCell>
+                <TableCell>3</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>1</TableCell>
+                <TableCell>2</TableCell>
+                <TableCell>3</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
         </Card>
       </div>
     </div>
