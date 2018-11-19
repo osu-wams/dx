59c9a19c5cb7ee37b967bcc5fcaaf9d6c837c4a2
mxcosu <mxcosu@users.noreply.github.com>
2018-11-19 09:48:55 -0800

    Added tests for helper functions. Small temporary modification for Button to deal with default props (#11)

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,5 +1,6 @@
 import format from 'date-fns/format';
 
+// Format any WORDS SENTENCE/word sentence/wOrD sEnTenCe to Title Case: Word Sentence
 export const titleCase = phrase =>
   phrase
     .toLowerCase()
@@ -7,14 +8,28 @@ export const titleCase = phrase =>
     .map(word => word.charAt(0).toUpperCase() + word.slice(1))
     .join(' ');
 
+// Format API time values to human friendly 12 hour am/pm format
 export function formatTime(hours) {
-  const hourEnd = hours.indexOf(':');
-  const H = hours.substr(0, hourEnd);
-  const h = H % 12 || 12;
-  const suffix = H < 12 ? 'am' : 'pm';
-  return h + hours.substr(hourEnd, 3) + suffix;
+  let result = 'Unexpected time format';
+
+  // Expected time format from API
+  const expectedFormat = /^\s*([01]?\d|2[0-3]):?([0-5]\d):?([0-5]\d)\s*$/;
+  const matches = hours.match(expectedFormat);
+
+  // Expected format matches user input, so we transform it
+  if (matches) {
+    const hourEnd = hours.indexOf(':');
+    const H = hours.substr(0, hourEnd);
+    const h = H % 12 || 12;
+    const suffix = H < 12 ? 'am' : 'pm';
+    result = h + hours.substr(hourEnd, 3) + suffix;
+  }
+  return result;
 }
 
+/* Format our day and time into preferred format
+using the npm package date-fns. We change number dates to:
+"December 09, 2018" format. */
 export function formatDate(date) {
   return format(date, 'MMMM DD, YYYY');
 }
---
+++ src/util/helpers.test.js
@@ -0,0 +1,34 @@
+import { titleCase, formatTime, formatDate } from './helpers';
+
+describe('titleCase', () => {
+  it('should transform sentences to title case', () => {
+    const input = 'WHAT IS GOING ON HERE';
+    const expected = 'What Is Going On Here';
+    expect(titleCase(input)).toBe(expected);
+  });
+});
+
+describe('formatTime', () => {
+  test.each`
+    input                | expected
+    ${'13:50:00'}        | ${'1:50pm'}
+    ${'12:00:00'}        | ${'12:00pm'}
+    ${'09:30:00'}        | ${'9:30am'}
+    ${'21:15:00'}        | ${'9:15pm'}
+    ${'Something bogus'} | ${'Unexpected time format'}
+    ${'435345:1'}        | ${'Unexpected time format'}
+  `('transform 24 hour time to simple 12 hour with am pm', ({ input, expected }) => {
+    expect(formatTime(input)).toBe(expected);
+  });
+});
+
+describe('formatDate', () => {
+  test.each`
+    input           | expected
+    ${'2018-12-06'} | ${'December 06, 2018'}
+    ${'2018-09-26'} | ${'September 26, 2018'}
+    ${'2018-01-11'} | ${'January 11, 2018'}
+  `('converts dates from API from numbers to human readabele', ({ input, expected }) => {
+    expect(formatDate(input)).toBe(expected);
+  });
+});
