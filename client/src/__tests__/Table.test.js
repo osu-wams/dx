38be694c28841478e9404e9e32b501ab76bffdb9
mxcosu <mxcosu@users.noreply.github.com>
2018-12-03 09:31:23 -0800

    Testing props for the Table: striped and variant (#14)

---
+++ client/src/__tests__/Table.test.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { render } from '../componentTestUtils';
+import { Table, TableBody, TableRow, TableCell } from '../components/layout/Table';
+
+const DefaultTable = () => (
+  <div>
+    <Table>
+      <caption>Default Table</caption>
+      <TableBody>
+        <TableRow>
+          <TableCell>One col</TableCell>
+          <TableCell>Two col</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  </div>
+);
+
+const BasicTable = () => (
+  <div>
+    <Table variant="basic">
+      <caption>Basic Table</caption>
+      <tbody>
+        <TableRow>
+          <TableCell>One col</TableCell>
+          <TableCell>Two col</TableCell>
+        </TableRow>
+      </tbody>
+    </Table>
+  </div>
+);
+
+const StripedTable = () => (
+  <div>
+    <Table variant="basic" striped>
+      <caption>Basic Table</caption>
+      <tbody>
+        <TableRow>
+          <TableCell>One col</TableCell>
+          <TableCell>Two col</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>One col</TableCell>
+          <TableCell>Two col</TableCell>
+        </TableRow>
+      </tbody>
+    </Table>
+  </div>
+);
+
+test('DefaultTable gets rendered with border around table', () => {
+  const { container } = render(<DefaultTable />);
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test('BasicTable no border around table element', () => {
+  const { container } = render(<BasicTable />);
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+test('StripedTable alternates background color on rows', () => {
+  const { container } = render(<StripedTable />);
+  expect(container.firstChild).toMatchSnapshot();
+});
