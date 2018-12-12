4cc729167362666f0e30905bb2d36e3b5ee09dab
Liv Vitale <LivVitale@users.noreply.github.com>
2018-12-12 13:06:33 -0800

    Wire up masquerade frontend (#25)

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle, faComment } from '@fortawesome/free-solid-svg-icons';
 import VisuallyHidden from '@reach/visually-hidden';
 import { Menu, MenuList, MenuButton, MenuItem, MenuLink } from '@reach/menu-button';
+import { ToastContainer, toast, Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import logo from '../../assets/logo.svg';
 import '@reach/menu-button/styles.css';
 import '@reach/dialog/styles.css';
@@ -46,17 +49,65 @@ const ChatButton = styled.button`
 `;
 
 const Header = () => {
-  const [mask, setMask] = useState(false);
+  const [showMasqueradeDialog, setShowMasqueradeDialog] = useState(false);
+  const [masqueradeId, setMasqueradeId] = useState('');
+
+  useEffect(() => {
+    loadMasqueradeId();
+  }, []);
+
+  const toggleMasqueradeDialog = () => setShowMasqueradeDialog(!showMasqueradeDialog);
+
+  const loadMasqueradeId = async () => {
+    try {
+      const { data } = await axios.get('/api/masquerade');
+      if (data.masqueradeId) {
+        setMasqueradeId(data.masqueradeId);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const masquerade = () => {
+    if (masqueradeId) {
+      axios
+        .post('/api/masquerade', { masqueradeId })
+        .then(() => {
+          toggleMasqueradeDialog();
+          toast.warn(`Masquerading as OSU ID ${masqueradeId}.`, { transition: Zoom });
+        })
+        .catch(err => console.log(err));
+    } else {
+      axios
+        .post('/api/masquerade')
+        .then(() => {
+          toggleMasqueradeDialog();
+          setMasqueradeId('');
+          toast.info('Masquerade session ended.', { transition: Zoom });
+        })
+        .catch(err => console.log(err));
+    }
+  };
+
+  const handleChange = e => {
+    setMasqueradeId(e.target.value);
+  };
+
+  const handleKeyDown = e => {
+    if (e.key === 'Enter') {
+      masquerade();
+    }
+  };
 
   const logout = () => {
     // logout logic needed
     logout();
   };
 
-  const toggleMask = () => setMask(!mask);
-
   return (
     <>
+      <ToastContainer />
       <NavbarWrapper>
         <ChatButton>
           <FontAwesomeIcon icon={faComment} size="2x" color="white" />
@@ -69,7 +120,7 @@ const Header = () => {
           </UserButton>
           <ProfileMenuList>
             <MenuItem onSelect={() => logout()}>Logout</MenuItem>
-            <MenuItem onSelect={() => toggleMask()}>Masquerade</MenuItem>
+            <MenuItem onSelect={toggleMasqueradeDialog}>Masquerade</MenuItem>
             <MenuLink to="profile" data-testid="profile-link">
               View Profile
             </MenuLink>
@@ -77,18 +128,26 @@ const Header = () => {
         </Menu>
       </NavbarWrapper>
 
-      {/* Masquerade Dialg Box */}
-      <MyDialog isOpen={mask} data-testid="masquerade-dialog">
+      {/* Masquerade Dialog Box */}
+      <MyDialog isOpen={showMasqueradeDialog} data-testid="masquerade-dialog">
         <h2>Log in as another user</h2>
-        <Label htmlFor="uuid">
-          Enter user OSU uuid
+        <Label htmlFor="osu-id">
+          Enter user OSU ID
           <br />
-          <Input type="text" id="uuid" />
+          <Input
+            type="text"
+            id="osu-id"
+            value={masqueradeId}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+          />
         </Label>
         <br />
         <br />
-        <Button type="submit">Masquerade</Button>
-        <Button type="cancel" bg="nimbus" fg="charcoal" outline onClick={() => toggleMask()}>
+        <Button type="submit" onClick={masquerade}>
+          Masquerade
+        </Button>
+        <Button type="cancel" bg="nimbus" fg="charcoal" outline onClick={toggleMasqueradeDialog}>
           Cancel
         </Button>
       </MyDialog>
