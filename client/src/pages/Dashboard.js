ad5f62caa6784d65b499fe0b4c1f7ed3f9cfd457
Charles Koll <kollch@oregonstate.edu>
2019-02-13 15:56:09 -0800

    Add FluffCard component as carousel card

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PageTitle from '../ui/PageTitle';
 import CourseScheduleCard from '../features/CourseScheduleCard';
 import UpcomingAssignments from '../features/UpcomingAssignments';
+import FluffCard from '../ui/FluffCard';
 
 const Dashboard = () => {
   return (
@@ -9,6 +10,19 @@ const Dashboard = () => {
       <PageTitle title="My OSU Dashboard" />
       <CourseScheduleCard />
       <UpcomingAssignments />
+      <FluffCard
+        items={[
+          {
+            title: 'Fluff Title 1',
+            text:
+              'Let us wax poetic about the beauty of the cheeseburger. The flavour gracefully dances across your palate like a majestic figure skater on ice.'
+          },
+          {
+            title: 'Fluff Title 2',
+            text: 'This is a shorter description.'
+          }
+        ]}
+      />
     </div>
   );
 };
---
+++ client/src/ui/FluffCard.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { CardBase } from './Card';
+import { Color, theme } from '../theme';
+import { useEffect } from 'react';
+
+const FluffCardWrapper = styled(CardBase)`
+  background-color: ${Color['stratosphere-100']};
+  padding: ${theme.spacing.unit * 2}px;
+`;
+
+const FluffCardTitle = styled.div`
+  color: ${Color['stratosphere-600']};
+  font-size: ${theme.fontSize['18']};
+  font-weight: 600;
+`;
+
+const FluffCardText = styled.div`
+  color: ${Color['stratosphere-500']};
+  font-size: ${theme.fontSize['16']};
+`;
+
+const CarouselButtons = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const CarouselButton = styled.button<{ active: boolean }>`
+  outline: none;
+  border: 1px solid ${Color['stratosphere-200']};
+  padding: 0;
+  margin: 4px;
+  height: 12px;
+  width: 12px;
+  border-radius: 50%;
+  background-color: ${props =>
+    props.active ? Color['stratosphere-400'] : Color['stratosphere-100']};
+`;
+
+const FluffCard = ({ items }) => {
+  // Set up some state for keeping track of which item to display
+  const [currentItemIndex, setCurrentItemIndex] = useState(0);
+  const [timer, setTimer] = useState<number | undefined>(undefined);
+
+  const startTimer = () => {
+    setTimer(
+      window.setInterval(() => {
+        setCurrentItemIndex(currentItemIndex => (currentItemIndex + 1) % items.length);
+      }, 5000)
+    );
+  };
+
+  useEffect(() => {
+    startTimer();
+    return () => window.clearInterval(timer);
+  }, []);
+
+  return (
+    <FluffCardWrapper>
+      <FluffCardTitle>{items[currentItemIndex].title}</FluffCardTitle>
+      <FluffCardText>{items[currentItemIndex].text}</FluffCardText>
+      <CarouselButtons>
+        {items.map((item, index) => (
+          <CarouselButton
+            onClick={() => {
+              setCurrentItemIndex(index);
+              clearInterval(timer);
+              startTimer();
+            }}
+            active={index === currentItemIndex}
+          />
+        ))}
+      </CarouselButtons>
+    </FluffCardWrapper>
+  );
+};
+
+export default FluffCard;
