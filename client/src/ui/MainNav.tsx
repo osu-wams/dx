be886b291ee304266e68d87f995a1b70bf8e85d2
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2019-02-15 16:43:11 -0800

    Main nav gradient and styles to match latest mockup. Hover effects

--- client/src/ui/MainNav.tsx
+++ client/src/ui/MainNav.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import { Link } from '@reach/router';
 import styled from 'styled-components';
-import { faHome, faGraduationCap, faHandsUsd, faHiking, faToolbox } from '@fortawesome/pro-light-svg-icons';
+import {
+  faHome,
+  faGraduationCap,
+  faHandsUsd,
+  faHiking,
+  faToolbox
+} from '@fortawesome/pro-light-svg-icons';
 import { Color } from '../theme';
 import Icon from './Icon';
 
 const Nav = styled.nav`
   display: flex;
   flex-direction: row;
-  margin-top: 24px;
+  margin: 24px auto 0 auto;
+  /* Side Gradients */
+  &:before,
+  &:after {
+    content: '';
+    height: 4.5rem;
+    position: absolute;
+    pointer-events: none;
+  }
+  &:before {
+    width: 3rem;
+    background: linear-gradient(to right, white 0.5rem, transparent);
+  }
+  &:after {
+    width: 3.5rem;
+    background: linear-gradient(to left, white 0.5rem, transparent);
+    right: 0;
+  }
 `;
 
 const NavLink = styled(Link)`
@@ -19,41 +42,53 @@ const NavLink = styled(Link)`
   padding: 0 4px;
   line-height: 30px;
   text-decoration: none;
-  color: ${Color["neutral-500"]};
-  &[aria-current], &[aria-current] > svg {
-    color: ${Color["orange-400"]};
-  }
-  /* select and hover styles */
-  &:active::after,
-  &:focus::after,
-  &:hover::after,
-  &[aria-current]::after {
-    width: 100%;
+  color: ${Color['neutral-500']};
+  &[aria-current],
+  &[aria-current] > svg,
+  &:active,
+  &:active > svg,
+  &:focus,
+  &:focus > svg,
+  &:hover,
+  &:hover > svg {
+    color: ${Color['orange-400']};
   }
   :first-child {
-    margin-left: 16px;
+    margin-left: 2rem;
   }
-  &::after {
-    content: '';
-    display: block;
-    width: 0;
-    height: 2px;
-    background: ${Color.white};
-    transition: width 0.3s;
+  :last-child {
+    margin-right: 2rem;
   }
   & > svg {
     font-size: 24px;
   }
 `;
 
-const MainNav = () => (
-  <Nav>
-    <NavLink to="/"><Icon icon={faHome} />Home</NavLink>
-    <NavLink to="academics"><Icon icon={faGraduationCap} />Academics</NavLink>
-    <NavLink to="finances"><Icon icon={faHandsUsd} />Finances</NavLink>
-    <NavLink to="experience"><Icon icon={faHiking} />Experience</NavLink>
-    <NavLink to="tools"><Icon icon={faToolbox} />Tools</NavLink>
-  </Nav>
-);
+const MainNav = () => {
+  return (
+    <Nav>
+      <NavLink to="/">
+        <Icon icon={faHome} />
+        Home
+      </NavLink>
+      <NavLink to="academics">
+        <Icon icon={faGraduationCap} />
+        Academics
+      </NavLink>
+      <NavLink to="finances">
+        <Icon icon={faHandsUsd} />
+        Finances
+      </NavLink>
+      <NavLink to="experience">
+        <Icon icon={faHiking} />
+        Experience
+      </NavLink>
+      <NavLink to="tools">
+        <Icon icon={faToolbox} />
+        Tools
+      </NavLink>
+    </Nav>
+  );
+};
 
 export default MainNav;
