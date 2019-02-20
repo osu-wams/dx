f6e22b5d5685cd952faca12988ae15847cdc52b3
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2019-02-19 17:13:46 -0800

    Initial work around service categories, unfinished

--- client/src/pages/Tools.tsx
+++ client/src/pages/Tools.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useState } from 'react';
 import PageTitle from '../ui/PageTitle';
+import ServicesCategories from '../features/services/ServiceCategories';
 
 const Tools: FC<{ initial?: {} }> = ({ initial = null }) => {
   const [api, setApi] = useState(initial);
@@ -7,6 +8,7 @@ const Tools: FC<{ initial?: {} }> = ({ initial = null }) => {
   return (
     <div data-testid="tools-page">
       <PageTitle title="Tools and Services" />
+      <ServicesCategories />
     </div>
   );
 };
