a6a2c5bb12ef1794079ce2f41ae20923fe9ca2cf
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2019-02-28 10:23:04 -0800

    Adding small delay so tests can be more consistent

--- cypress/e2e/mainNav.ts
+++ cypress/e2e/mainNav.ts
@@ -1,39 +1,42 @@
 describe('Main navigation', () => {
-    beforeEach(() => {
-      cy.visit('/')
-    })
+  // Tests were inconsistent before adding this load application and 3 second delay.
+  it('Loads the homepage', () => {
+    cy.visit('/')
+      .getByTestId('dashboard-page')
+      .wait(3000);
+  });
 
-    it('Can navigate to the Academics page', () => {
-      cy.get('[href="/academics"]')
-        .click()
-        .getByTestId('academics-page')
-        .should('exist')
-    })
+  it('Can navigate to the Academics page', () => {
+    cy.get('[href="/academics"]')
+      .click()
+      .getByTestId('academics-page');
+  });
 
-    it('Can navigate to the Finances page', () => {
-      cy.get('[href="/finances"]')
-        .click()
-        .getByTestId('finances-page')
-        .should('exist')
-    })
+  it('Can navigate to the Finances page', () => {
+    //cy.visit('/');
+    cy.get('[href="/finances"]')
+      .click()
+      .getByTestId('finances-page');
+  });
 
-    it('Can navigate to the Experience page', () => {
-      cy.get('[href="/experience"]')
-        .click()
-        .getByTestId('experience-page')
-        .should('exist')
-    })
+  it('Can navigate to the Experience page', () => {
+    //cy.visit('/');
+    cy.get('[href="/experience"]')
+      .click()
+      .getByTestId('experience-page');
+  });
 
-    it('Can navigate to the Tools page', () => {
-      cy.get('[href="/tools"]')
-        .click()
-        .getByTestId('tools-page')
-        .should('exist')
-    })
+  it('Can navigate to the Tools page', () => {
+    //cy.visit('/');
+    cy.get('[href="/tools"]')
+      .click()
+      .getByTestId('tools-page');
+  });
 
-    it('Can navigate to the Homepage', () => {
-      cy.getByText('Home')
-        .click()
-        .getByTestId('dashboard-page')
-    })
-  })
+  it('Can navigate back to the Homepage', () => {
+    //cy.visit('/academics');
+    cy.getByText('Home')
+      .click()
+      .getByTestId('dashboard-page');
+  });
+});
