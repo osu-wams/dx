cad169d080175fb34bb77ac1464273830bdc803f
Charles Koll <kollch@oregonstate.edu>
2019-04-30 16:12:38 -0700

    Add AnnouncementContainer component and reformat announcement api for multiple inputs

--- client/src/pages/Finances.tsx
+++ client/src/pages/Finances.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PageTitle from '../ui/PageTitle';
+import AnnouncementContainer from '../ui/AnnouncementContainer';
 
 const Finances = () => {
   return (
     <div data-testid="finances-page">
       <PageTitle title="Financial Information" />
+      <AnnouncementContainer type="financial" />
     </div>
   );
 };
--- server/api/announcements.js
+++ server/api/announcements.js
@@ -3,28 +3,54 @@ const request = require('request-promise');
 
 const baseUrl = 'http://dev-api-dx.pantheonsite.io';
 const announcementsUrl = `${baseUrl}/jsonapi/node/announcement?include=field_announcement_image&sort=-created`;
+const queueUrl = `${baseUrl}/jsonapi/entity_subqueue/announcements`;
 
 const announcements = Router();
 
+const getData = async url => {
+  const { data, included } = await request.get(url, { json: true });
+  if (included) {
+    included.forEach(item => {
+      const matchingAnnouncement = data.find(e => {
+        return (
+          e.relationships.field_announcement_image.data &&
+          e.relationships.field_announcement_image.data.id === item.id
+        );
+      });
+      if (matchingAnnouncement) {
+        data[data.indexOf(matchingAnnouncement)].attributes.background_image = `${baseUrl}${
+          item.attributes.uri.url
+        }`;
+      }
+    });
+  }
+  return data;
+};
+
 announcements.get('/', async (req, res) => {
   try {
-    const { data, included } = await request.get(announcementsUrl, { json: true });
-    if (included) {
-      included.forEach(item => {
-        const matchingAnnouncement = data.find(e => {
-          return (
-            e.relationships.field_announcement_image.data &&
-            e.relationships.field_announcement_image.data.id === item.id
-          );
-        });
-        if (matchingAnnouncement) {
-          data[data.indexOf(matchingAnnouncement)].attributes.background_image = `${baseUrl}${
-            item.attributes.uri.url
-          }`;
-        }
-      });
-    }
-    res.send(data);
+    const result = await getData(announcementsUrl);
+    res.send(result);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+announcements.get('/academic', async (req, res) => {
+  const academicUrl = `${queueUrl}/9ff07e4b-ec28-4dfb-8b75-9bbc1ef9d7cb/items?include=field_announcement_image`;
+  try {
+    const result = await getData(academicUrl);
+    res.send(result);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+announcements.get('/financial', async (req, res) => {
+  const financialUrl = `${queueUrl}/9e3a07b8-4174-4979-990c-c114d2410c29/items?include=field_announcement_image`;
+  try {
+    const result = await getData(financialUrl);
+    res.send(result);
   } catch (err) {
     res.status(500).send(err);
   }
