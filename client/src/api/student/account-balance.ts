a7c24caf8ac5d2150a4331e68df3c1bf3d269cdf
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2019-05-20 15:24:56 -0700

    Adding types for account balance API response

---
+++ client/src/api/student/account-balance.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const getAccountBalance = (): Promise<IAccountBalance> =>
+  axios.get('/api/student/account-balance').then(res => res.data);
+
+interface IAccountBalance {
+  attributes: IAccountBalanceAttributes;
+  id: number;
+  links: { self: null };
+  type: string;
+}
+
+export type IAccountBalanceAttributes = {
+  currentBalance: number;
+};
+
+export default getAccountBalance;
--- client/src/api/student/index.ts
+++ client/src/api/student/index.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import getCourseSchedule from './course-schedule';
 import getGrades from './grades';
+import getAccountBalance from './account-balance';
 
-export { getCourseSchedule, getGrades };
+export { getCourseSchedule, getGrades, getAccountBalance };
 
 // Todo: Replace rest of API calls with properly-typed versions.
 
@@ -11,9 +12,6 @@ export const getUpcomingAssignments = () =>
 
 export const getAcademicStatus = () =>
   axios.get(`/api/student/academic-status`).then(res => res.data);
-
-export const getAccountBalance = () =>
-  axios.get('/api/student/account-balance').then(res => res.data);
 
 export const getAccountTransactions = () =>
   axios.get('/api/student/account-transactions').then(res => res.data);
