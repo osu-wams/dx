a67c69ef2f94eb932c7f24b6d1be0096df00ebbc
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2019-05-21 13:48:45 -0700

    PageGrid component and snapshot testing for it. Refactoring pages were we used the old one

---
+++ client/src/ui/PageGrid.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+/* Page Grid
+ * Single column for mobile. 2 column for Desktop
+ * Provides row-span and col-span classes to modify width or height of children elements
+ */
+
+const PageGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 16px;
+    div {
+      grid-row: span 3;
+    }
+    .row-span-1 {
+      grid-row: span 1;
+    }
+    .row-span-2 {
+      grid-row: span 2;
+    }
+    .row-span-4 {
+      grid-row: span 4;
+    }
+    .row-span-5 {
+      grid-row: span 5;
+    }
+    .col-span-2 {
+      grid-column-start: 1;
+      grid-column-end: 3;
+    }
+  }
+`;
+
+export default PageGrid;
