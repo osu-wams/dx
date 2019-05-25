2514edecee80d6c5006c36bad121a3e3382620c9
Mauricio <mauricio.cordoba@oregonstate.edu>
2019-05-25 15:08:50 -0700

    Test coverage report reinstated, commenting out test code that was causing warning, removing unused component FluffCard

--- client/src/ui/__tests__/Header.test.tsx
+++ client/src/ui/__tests__/Header.test.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import axios from 'axios';
 import { act } from 'react-dom/test-utils';
-import MockAdapter from 'axios-mock-adapter';
 import { render, fireEvent, waitForElement } from 'react-testing-library';
 import Header from '../Header';
 import App from '../../App';
 
-const mockAxios = new MockAdapter(axios);
-
-mockAxios.onGet(/\/api\/masquerade/).reply(200, { masqueradeId: null });
-mockAxios.onPost(/\/api\/masquerade/).reply(200, '');
 
 test('renders', () => {
   render(<Header />);
