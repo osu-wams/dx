c48a593c54ff867c0d65eec675abc16296822f45
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2019-06-05 12:52:34 -0700

    Persons main API endpoint and tests

--- server/api/__tests__/persons.test.js
+++ server/api/__tests__/persons.test.js
@@ -2,6 +2,7 @@ const supertest = require('supertest');
 const nock = require('nock');
 const config = require('config');
 const app = require('../../index');
+const { personsData } = require('../__mocks__/persons.data');
 
 jest.mock('../util.js');
 
@@ -14,9 +15,33 @@ describe('/api/persons', () => {
     await request.get('/login');
   });
 
+  describe('/', () => {
+    it('should return person general information', async () => {
+      // Mock response from Apigee
+      nock(APIGEE_BASE_URL)
+        .get(/persons\/[0-9]/)
+        .query(true)
+        .reply(200, personsData);
+
+      const res = await request.get('/api/persons/');
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.attributes.firstName).toEqual('Testo');
+    });
+
+    it('should return an error if the user is not logged in', async () => {
+      // Clear session data - we don't want to be logged in
+      request = supertest.agent(app);
+
+      const res = await request.get('/api/persons/');
+      expect(res.statusCode).toEqual(401);
+      expect(res.error.text).toEqual('Unauthorized');
+    });
+  });
+
+  // Meal Plan Balances
   describe('/meal-plans', () => {
     it('should return meal plans', async () => {
-      // Mock response from Handshake
+      // Mock response from apigee
       nock(APIGEE_BASE_URL)
         .get(/persons\/[0-9]+\/meal-plans/)
         .query(true)
--- server/api/persons.js
+++ server/api/persons.js
@@ -8,8 +8,23 @@ const config = require('config');
 const { getToken } = require('./util');
 
 const BASE_URL = `${config.get('osuApi.baseUrl')}/persons`;
-
 const persons = new Router();
+
+// Main endpoint with general data about the person
+persons.get('/', async (req, res) => {
+  try {
+    const bearerToken = await getToken();
+    const apiResponse = await request({
+      method: 'GET',
+      url: `${BASE_URL}/${req.user.masqueradeId || req.user.osuId}`,
+      auth: { bearer: bearerToken },
+      json: true
+    });
+    res.send(apiResponse.data);
+  } catch (err) {
+    res.status(500).send('Unable to retrieve person information.');
+  }
+});
 
 // Meal Plan by osuuid - Apigee endpoint
 persons.get('/meal-plans', async (req, res) => {
