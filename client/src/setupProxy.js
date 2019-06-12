c02863ea87f81b3b5a0e70890902eb273842d99e
Matthew Brabham <matthew.brabham@oregonstate.edu>
2019-06-12 11:06:21 -0700

    Started working on saml logout

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,4 +5,5 @@ const proxy = require('http-proxy-middleware');
 module.exports = app => {
   app.use(proxy('/api', { target: 'http://localhost:4000/' }));
   app.use(proxy('/login', { target: 'http://localhost:4000/' }));
+  app.use(proxy('/logout', { target: 'http://localhost:4000/' }));
 };
