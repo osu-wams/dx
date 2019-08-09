327bc8eb701e67c53b5bc6a7d4d9aca6f09da251
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2019-08-09 11:47:46 -0700

    Authorize Canvas in CourseScheduleCard component done, and modifications made to acomodate icons with background colors

--- src/ui/Icon.tsx
+++ src/ui/Icon.tsx
@@ -2,8 +2,14 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Color } from '../theme';
 
-const Icon = styled(FontAwesomeIcon)`
+type IconProps = {
+  bg?: Color;
+  color?: Color;
+};
+const Icon = styled(FontAwesomeIcon)<IconProps>`
   color: ${props => props.color || Color['neutral-400']};
+  background-color: ${props => props.bg || 'transparent'};
+  padding: ${props => (props.bg ? '.5rem' : '0')};
 `;
 
 export default Icon;
