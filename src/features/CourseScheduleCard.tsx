b7eaf06dccdc054f3ce3db7e2f7804276bedfcc4
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2019-08-09 17:03:56 -0700

    New styling for day button menu, starting separating logic from main CourseScheduleCard

--- src/features/CourseScheduleCard.tsx
+++ src/features/CourseScheduleCard.tsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect, useContext, useMemo } from 'react';
-import styled from 'styled-components';
 import { faFileAlt, faMapMarkerAlt, faArrowRight } from '@fortawesome/pro-light-svg-icons';
-import { addDays, eachDay, format, isSameDay } from 'date-fns';
+import { format, isSameDay } from 'date-fns';
 import generateId from 'uuid/v4';
-import { CardBase } from '../ui/Card';
 import {
   List,
   ListItem,
@@ -14,7 +12,7 @@ import {
   ListItemContentLink
 } from '../ui/List';
 import Icon from '../ui/Icon';
-import { theme, Color } from '../theme';
+import { Color } from '../theme';
 import excitedCalendarIcon from '../assets/excited-calendar.svg';
 import { getCourseSchedule } from '../api/student';
 import { getPlannerItems } from '../api/student/planner-items';
@@ -24,6 +22,9 @@ import VisuallyHidden from '@reach/visually-hidden';
 import Url from '../util/externalUrls.data';
 import { UserContext } from '../App';
 import { AuthorizeCanvasCompact } from './canvas/AuthorizeCanvasCompact';
+import { getNextFiveDays, getDayShortcode } from './schedule/schedule-utils';
+import { DayMenu } from './schedule/DayMenu';
+import { Header, Card, CardSection, NoCoursesImage, NoCoursesText, SectionHeader } from './schedule/ScheduleStyles';
 
 /**
  * Course Schedule Card
@@ -115,49 +116,38 @@ const CourseScheduleCard = () => {
 
   return (
     <Card>
-      <Header>This Week</Header>
-      <DayList>
-        {nextFiveDays.map(day => (
-          <Day
-            key={day.toUTCString()}
-            onClick={() => setSelectedDay(day)}
-            selected={isSameDay(day, selectedDay)}
-          >
-            <span>{daysWithEvents.includes(day) ? '\u2022' : ''}</span>
-            <span>{format(day, 'ddd')}</span>
-            <span>{format(day, 'D')}</span>
-          </Day>
-        ))}
-      </DayList>
+      <Header>{format(selectedDay, 'dddd MMM Do')} | Day at a Glance </Header>
+      
+      <DayMenu nextFiveDays={nextFiveDays} selectedDay={selectedDay} setSelectedDay={setSelectedDay} daysWithEvents={daysWithEvents} />
       
       {selectedCourses.length === 0 && selectedPlannerItems.length === 0 && <EmptyState />}
       
 
-        <CardSection>
-          <SectionHeader>Assignments</SectionHeader>
-          <List>
-            {user.isCanvasOptIn !== undefined && !user.isCanvasOptIn && (
-              <AuthorizeCanvasCompact />
-            )}
-            {user.isCanvasOptIn && selectedPlannerItems.length > 0 && selectedPlannerItems.map(
-              ({ plannable_id, html_url, plannable_type, plannable: { title, due_at } }) => (
-                <ListItem key={plannable_id}>
-                  <ListItemContentLink href={Url.canvas.main + html_url}>
-                    <Icon icon={faFileAlt} color={Color['orange-200']} />
-                    <ListItemText>
-                      <ListItemHeader>{title} </ListItemHeader>
-                      <ListItemDescription>
-                        {plannable_type !== 'announcement'
-                          ? `Due today at ${format(due_at, 'h:mma')}`
-                          : ''}
-                      </ListItemDescription>
-                    </ListItemText>
-                  </ListItemContentLink>
-                </ListItem>
-              )
-            )}
-          </List>
-        </CardSection>
+      <CardSection>
+        <SectionHeader>Assignments</SectionHeader>
+        <List>
+          {user.isCanvasOptIn !== undefined && !user.isCanvasOptIn && (
+            <AuthorizeCanvasCompact />
+          )}
+          {user.isCanvasOptIn && selectedPlannerItems.length > 0 && selectedPlannerItems.map(
+            ({ plannable_id, html_url, plannable_type, plannable: { title, due_at } }) => (
+              <ListItem key={plannable_id}>
+                <ListItemContentLink href={Url.canvas.main + html_url}>
+                  <Icon icon={faFileAlt} color={Color['orange-200']} />
+                  <ListItemText>
+                    <ListItemHeader>{title} </ListItemHeader>
+                    <ListItemDescription>
+                      {plannable_type !== 'announcement'
+                        ? `Due today at ${format(due_at, 'h:mma')}`
+                        : ''}
+                    </ListItemDescription>
+                  </ListItemText>
+                </ListItemContentLink>
+              </ListItem>
+            )
+          )}
+        </List>
+      </CardSection>
 
       {selectedCourses.length > 0 && (
         <CardSection>
@@ -200,24 +190,7 @@ const CourseScheduleCard = () => {
   );
 };
 
-/**
- * Utility functions
- */
-const getNextFiveDays = () => {
-  let rangeStart = new Date();
-  let rangeEnd = addDays(rangeStart, 4);
-  let nextFiveDays = eachDay(rangeStart, rangeEnd);
 
-  return nextFiveDays;
-};
-
-const getDayShortcode = (date: Date) => {
-  let twoLetterShortcodes = ['Th', 'Sa', 'Su'];
-
-  let shortcode = format(date, 'dddd').substr(0, 2);
-  shortcode = twoLetterShortcodes.includes(shortcode) ? shortcode : shortcode.substr(0, 1);
-  return shortcode;
-};
 
 const EmptyState = () => (
   <>
@@ -231,108 +204,5 @@ const EmptyState = () => (
     </NoCoursesText>
   </>
 );
-
-/**
- * Styling
- */
-
-const CardSection = styled.div`
-  & + div {
-    margin-top: 1.6rem;
-  }
-`;
-
-const Card = styled(CardBase)`
-  padding: ${theme.spacing.unit * 2}px;
-`;
-
-const Header = styled.div`
-  color: ${Color['neutral-600']};
-  font-size: ${theme.fontSize[18]};
-  margin-bottom: ${theme.spacing.unit * 2}px;
-`;
-
-type selectedBtn = boolean;
-
-const Day = styled.button<{ selected: selectedBtn }>`
-  background: none;
-  border: none;
-  padding: 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  cursor: pointer;
-
-  & > span:first-child {
-    color: ${Color['orange-500']};
-    font-weight: bold;
-    font-size: ${theme.fontSize[20]};
-    line-height: 18px;
-  }
-
-  & > span:nth-child(2) {
-    color: ${Color['neutral-550']};
-    font-weight: bold;
-    font-size: ${theme.fontSize[12]};
-    text-transform: uppercase;
-    margin-bottom: ${theme.spacing.unit * 0.5}px;
-  }
-
-  & > span:last-child {
-    color: ${Color['neutral-700']};
-    line-height: 20px;
-    font-size: ${theme.fontSize[24]};
-  }
-
-  ${props =>
-    props.selected &&
-    `
-    & > span {
-      color: ${Color['orange-400']} !important;
-    }
-  `}
-`;
-
-const DayList = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: ${theme.spacing.unit * 2}px;
-`;
-
-const NoCoursesImage = styled.img`
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: ${theme.spacing.unit * 2}px;
-  height: 100px;
-`;
-
-const NoCoursesText = styled.div`
-  font-size: ${theme.fontSize[14]};
-  text-align: center;
-  padding: 0 ${theme.spacing.unit * 2}px;
-
-  & > a {
-    color: ${Color['orange-400']};
-    margin-left: ${theme.spacing.unit / 2}px;
-    text-decoration: none;
-    font-weight: 600;
-  }
-
-  & > a:hover {
-    text-decoration: underline;
-  }
-
-  & > a > svg {
-    margin-left: ${theme.spacing.unit}px;
-  }
-`;
-
-const SectionHeader = styled.div`
-  color: ${Color['neutral-550']};
-  font-weight: 600;
-  margin-bottom: ${theme.spacing.unit}px;
-`;
 
 export default CourseScheduleCard;
