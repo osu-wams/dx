8496560a8f57c8bc5112d9db8fe7d6580304aa83
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2019-08-12 14:33:56 -0700

    Adding a renderWithUserContext test helper utility, and moving PlannerItems card to the Academics page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PageTitle from '../ui/PageTitle';
 import ScheduleCard from '../features/ScheduleCard';
-import PlannerItems from '../features/PlannerItems';
 import EventCardContainer from '../ui/EventCardContainer';
 import PageGrid from '../ui/PageGrid';
 
@@ -11,7 +10,6 @@ const Dashboard = () => {
       <PageTitle title="My OSU Dashboard" />
       <PageGrid>
         <ScheduleCard />
-        <PlannerItems />
         <EventCardContainer className="col-span-2" />
       </PageGrid>
     </div>
