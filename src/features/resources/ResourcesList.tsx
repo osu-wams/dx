5bf6f1bf6852b9494da7af947a05c143baf5533a
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2019-08-27 13:06:28 -0700

    animation and loading states for resources

--- src/features/resources/ResourcesList.tsx
+++ src/features/resources/ResourcesList.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 import { faCube } from '@fortawesome/pro-light-svg-icons';
+import { PoseGroup } from 'react-pose';
 import Icon from '../../ui/Icon';
-import { List, ListItem, ListItemContentLink } from '../../ui/List';
+import { List, ListItemAnimated, ListItemContentLink } from '../../ui/List';
 import { Color, theme } from '../../theme';
 import { IResourceResult } from '../../api/resources';
 
 const ResourcesList: React.FC<{ resources: IResourceResult[] }> = ({ resources }) => (
   <List>
-    {resources.length > 0 &&
-      resources.map((resource: IResourceResult) => (
-        <ListItem spaced key={resource.id}>
-          <ListItemContentLink spaced href={resource.uri} target="_blank">
-            {resource.icon !== undefined ? (
-              <ResourceImg src={resource.icon} alt="" />
-            ) : (
-              <ResourceIcon icon={faCube} color={Color.black} />
-            )}
-            <ResourceName>{resource.title}</ResourceName>
-          </ListItemContentLink>
-        </ListItem>
-      ))}
+    <PoseGroup>
+      {resources.length > 0 &&
+        resources.map((resource: IResourceResult) => (
+          <ListItemAnimated spaced key={resource.id} pose="closed">
+            <ListItemContentLink spaced href={resource.uri} target="_blank">
+              {resource.icon !== undefined ? (
+                <ResourceImg src={resource.icon} alt="" />
+              ) : (
+                <ResourceIcon icon={faCube} color={Color.black} />
+              )}
+              <ResourceName>{resource.title}</ResourceName>
+            </ListItemContentLink>
+          </ListItemAnimated>
+        ))}
+    </PoseGroup>
   </List>
 );
 
