d6a3be33a694d48cd7b33c565a20fa84d3597683
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2019-08-29 16:46:44 -0700

    Adds test to PlannerItems that checks that there are no assignments present

--- src/features/PlannerItems.tsx
+++ src/features/PlannerItems.tsx
@@ -4,7 +4,14 @@ import { format } from 'date-fns';
 import { faFileEdit } from '@fortawesome/pro-light-svg-icons';
 import { Card, CardHeader, CardContent, CardFooter, CardIcon } from '../ui/Card';
 import Icon from '../ui/Icon';
-import { List, ListItem, ListItemContentLink, ListItemDescription, ListItemHeader, ListItemText } from '../ui/List';
+import {
+  List,
+  ListItem,
+  ListItemContentLink,
+  ListItemDescription,
+  ListItemHeader,
+  ListItemText
+} from '../ui/List';
 import { getPlannerItems } from '../api/student/planner-items';
 import { AuthorizeCanvas } from '../features/canvas/AuthorizeCanvas';
 import { Color } from '../theme';
@@ -18,14 +25,14 @@ import { UserContext } from '../App';
  * Displays upcoming assignments from Canvas.
  */
 const PlannerItems = () => {
-  const [ plannerItems, setPlannerItems ] = useState([]);
+  const [plannerItems, setPlannerItems] = useState([]);
   const user = useContext<any>(UserContext);
-  const [ plannerItemsLoading, setPlannerItemsLoading ] = useState<boolean>(true);
+  const [plannerItemsLoading, setPlannerItemsLoading] = useState<boolean>(true);
 
   // Populate assignment data for current user
   useEffect(() => {
     getPlannerItems()
-      .then((data) => {
+      .then(data => {
         setPlannerItems(data);
         setPlannerItemsLoading(false);
       })
@@ -34,7 +41,10 @@ const PlannerItems = () => {
 
   return (
     <Card>
-      <CardHeader title="Canvas" badge={<CardIcon icon={faFileEdit} count={plannerItems.length} />} />
+      <CardHeader
+        title="Canvas"
+        badge={<CardIcon icon={faFileEdit} count={plannerItems.length} />}
+      />
       <CardContent>
         {/* Show upcoming assignments if any exist, otherwise show empty state. */}
         {!user.isCanvasOptIn && user.isCanvasOptIn !== undefined && <AuthorizeCanvas />}
@@ -42,30 +52,32 @@ const PlannerItems = () => {
         {plannerItemsLoading && <Skeleton count={5} />}
         {plannerItems.length && user.isCanvasOptIn === true ? (
           <List>
-            {plannerItems.map(({ plannable_id, plannable_type, html_url, plannable: { title, due_at } }) => (
-              <ListItem key={plannable_id}>
-                <ListItemContentLink href={Url.canvas.main + html_url} target="_blank">
-                  <Icon icon={faFileEdit} color={Color['orange-200']} />
-                  <ListItemText>
-                    <ListItemHeader>{title}</ListItemHeader>
-                    <ListItemDescription>
-                      {plannable_type !== ('calendar_event' || 'announcement') ? (
-                        `Due ${format(due_at, 'MMM Do [at] h:mma')}`
-                      ) : (
-                        ''
-                      )}
-                    </ListItemDescription>
-                  </ListItemText>
-                </ListItemContentLink>
-              </ListItem>
-            ))}
+            {plannerItems.map(
+              ({ plannable_id, plannable_type, html_url, plannable: { title, due_at } }) => (
+                <ListItem key={plannable_id}>
+                  <ListItemContentLink href={Url.canvas.main + html_url} target="_blank">
+                    <Icon icon={faFileEdit} color={Color['orange-200']} />
+                    <ListItemText>
+                      <ListItemHeader>{title}</ListItemHeader>
+                      <ListItemDescription>
+                        {plannable_type !== ('calendar_event' || 'announcement')
+                          ? `Due ${format(due_at, 'MMM Do [at] h:mma')}`
+                          : ''}
+                      </ListItemDescription>
+                    </ListItemText>
+                  </ListItemContentLink>
+                </ListItem>
+              )
+            )}
           </List>
         ) : (
           !plannerItemsLoading && (user.isCanvasOptIn === true && <EmptyState />)
         )}
       </CardContent>
       <CardFooter>
-        {user.isCanvasOptIn === true && <ExternalLink href={Url.canvas.main}>View all in Canvas</ExternalLink>}
+        {user.isCanvasOptIn === true && (
+          <ExternalLink href={Url.canvas.main}>View all in Canvas</ExternalLink>
+        )}
       </CardFooter>
     </Card>
   );
@@ -74,6 +86,6 @@ const PlannerItems = () => {
 // <ListItemContent as="a" href={url} target="_blank">
 
 // Todo: Replace with actual empty state when ready in mockups.
-const EmptyState = () => <span>NO ASSIGNMENTS </span>;
+const EmptyState = () => <span>NO ASSIGNMENTS</span>;
 
 export default PlannerItems;
