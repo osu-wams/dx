ff263e40ebed8ae19e135900699b82afad5ff94d
Josh Gum <revgum@gmail.com>
2019-09-12 07:45:27 -0700

    calculates credit hours attempted from current course schedule

--- src/features/__tests__/StudentEnrolledCredits.test.tsx
+++ src/features/__tests__/StudentEnrolledCredits.test.tsx
@@ -3,22 +3,24 @@ import { waitForElement } from '@testing-library/react';
 import { render } from '../../util/test-utils';
 import StudentEnrolledCredits from '../academic-overview/StudentEnrolledCredits';
 
-const mockGetAcademicStatus = jest.fn();
+const mockGetCourseSchedule = jest.fn();
 
-jest.mock('../../api/student/academic-status', () => ({
-  getAcademicStatus: () => mockGetAcademicStatus()
+jest.mock('../../api/student/course-schedule', () => ({
+  getCourseSchedule: () => mockGetCourseSchedule()
 }));
 
 describe('<StudentEnrolledCredits />', () => {
   it('should render and have the approriate standing', async () => {
-    mockGetAcademicStatus.mockResolvedValue(Promise.resolve({ creditHoursAttempted: 800 }));
+    mockGetCourseSchedule.mockResolvedValue(
+      Promise.resolve([{ attributes: { creditHours: 5 } }, { attributes: { creditHours: 2 } }])
+    );
     const { getByText } = render(<StudentEnrolledCredits />);
-    const element = await waitForElement(() => getByText('800'));
+    const element = await waitForElement(() => getByText('7'));
     expect(element).toBeInTheDocument();
   });
 
   it('should return appropriate text when data is empty', async () => {
-    mockGetAcademicStatus.mockResolvedValue(Promise.resolve({}));
+    mockGetCourseSchedule.mockResolvedValue(Promise.resolve([]));
     const { getByText } = render(<StudentEnrolledCredits />);
     const element = await waitForElement(() => getByText('0'));
     expect(element).toBeInTheDocument();
