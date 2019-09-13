576dcbd92f0173e5de4e00eb1d8cb828840b2d19
Charles Koll <kollch@oregonstate.edu>
2019-09-13 15:22:07 -0700

    Filter resources by audience

--- src/api/resources.ts
+++ src/api/resources.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import useAPICall from './useAPICall';
+import { getClassification } from '../api/student/classification';
 
 export interface IResourceResult {
   id: string;
@@ -14,13 +15,54 @@ export interface ICategory {
   icon: string;
 }
 
+const userClassifications = async () => {
+  const classifications = await getClassification();
+  const results: string[] = [];
+  if (classifications.attributes.level === 'Graduate') {
+    results.push('Graduate Student');
+  }
+  if (classifications.attributes.campus === 'Dist. Degree Corvallis Student') {
+    results.push('Ecampus');
+  } else if (classifications.attributes.campus === 'Oregon State - Cascades') {
+    results.push('Bend');
+  } else {
+    results.push('Corvallis');
+  }
+  if (classifications.attributes.classification === 'Freshman') {
+    results.push('First Year');
+  }
+  if (classifications.attributes.isInternational) {
+    results.push('International Student');
+  }
+  return results;
+};
+
+const filterResourcesForUser = async (
+  resources: (IResourceResult & { audiences: string[] })[]
+): Promise<IResourceResult[]> => {
+  const classifications = await userClassifications();
+  return resources
+    .filter(e => e.audiences.some(audience => classifications.includes(audience)))
+    .map(({ audiences, ...resourceAttribs }) => {
+      return { ...resourceAttribs };
+    });
+};
+
 /**
  * Resources
  */
 const getResources = (query: string): Promise<IResourceResult[]> =>
   axios.get(`/api/resources${query ? `${query}` : ''}`).then(res => res.data);
 const useResources = (query: string) => {
-  return useAPICall<IResourceResult[]>(getResources, query, d => d, []);
+  return useAPICall<IResourceResult[]>(
+    getResources,
+    query,
+    async d => {
+      const transformed = await filterResourcesForUser(d);
+      return transformed;
+    },
+    []
+  );
 };
 
 /**
@@ -29,7 +71,15 @@ const useResources = (query: string) => {
 const getResourcesByQuery = (query: string): Promise<IResourceResult[]> =>
   axios.get(`/api/resources${query ? `?query=${query}` : ''}`).then(res => res.data);
 const useResourcesByQuery = (query: string) => {
-  return useAPICall<IResourceResult[]>(getResourcesByQuery, query, d => d, []);
+  return useAPICall<IResourceResult[]>(
+    getResourcesByQuery,
+    query,
+    async d => {
+      const transformed = await filterResourcesForUser(d);
+      return transformed;
+    },
+    []
+  );
 };
 
 /**
@@ -40,7 +90,15 @@ const getResourcesByCategory = (categoryId: string): Promise<IResourceResult[]> 
     .get(`/api/resources${categoryId !== 'all' ? `?category=${categoryId}` : ''}`)
     .then(res => res.data);
 const useResourcesByCategory = (categoryId: string) => {
-  return useAPICall<IResourceResult[]>(getResourcesByCategory, categoryId, d => d, []);
+  return useAPICall<IResourceResult[]>(
+    getResourcesByCategory,
+    categoryId,
+    async d => {
+      const transformed = await filterResourcesForUser(d);
+      return transformed;
+    },
+    []
+  );
 };
 
 /**
@@ -49,7 +107,15 @@ const useResourcesByCategory = (categoryId: string) => {
 const getResourcesByQueue = (category: string): Promise<IResourceResult[]> =>
   axios.get(`/api/resources/category/${category}`).then(res => res.data);
 const useResourcesByQueue = (category: string) =>
-  useAPICall<IResourceResult[]>(getResourcesByQueue, category, d => d, []);
+  useAPICall<IResourceResult[]>(
+    getResourcesByQueue,
+    category,
+    async d => {
+      const transformed = await filterResourcesForUser(d);
+      return transformed;
+    },
+    []
+  );
 
 /**
  * Categories
