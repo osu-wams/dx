b5481a57fe6f114440f32857d89b7eeea45cc156
Nick Piatt <developer.piatt@gmail.com>
2019-09-19 10:32:42 -0700

    wrote tests to make sure that all the expected titles are being displayed

--- src/pages/__tests__/Dashboard.test.tsx
+++ src/pages/__tests__/Dashboard.test.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import { renderWithUserContext } from '../../util/test-utils';
+import { waitForElement } from '@testing-library/react';
+import { render, renderWithUserContext } from '../../util/test-utils';
 import Dashboard from '../Dashboard';
 
 test('renders', () => {
   const { getByTestId } = renderWithUserContext(<Dashboard />);
   expect(getByTestId('dashboard-page')).toBeInTheDocument();
 });
+
+test('should display the title Student Dashboard', async () => {
+  const { getByText } = render(<Dashboard />);
+  await waitForElement(() => getByText('Student Dashboard'));
+});
+
