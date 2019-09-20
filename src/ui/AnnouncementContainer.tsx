d6ebb731afc4635d26e8ef3b495bec63ec2522e4
Patenaude, Christopher T <patenauc@oregonstate.edu>
2019-09-20 12:25:17 -0700

    Implement useUser and useInfobutton hooks

--- src/ui/AnnouncementContainer.tsx
+++ src/ui/AnnouncementContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { useAnnouncements } from '../api/announcements';
 import EventCard from './EventCard';
