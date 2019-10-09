ecc0f2a5e66bb965d93dea87f8ad8cd1acbe25d7
Josh Gum <revgum@gmail.com>
2019-10-09 15:21:24 -0700

    adds debug flag for google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,13 @@ import App from './App';
 // Initialize Google Analytics
 const isDevelopment = process.env.NODE_ENV === 'development';
 const isTest = process.env.NODE_ENV === 'test';
+const isGADebug = process.env.REACT_APP_GA_DEBUG === 'true';
 
 // When running tests, intialize in ./setupTests.js instead
 if (!isTest) {
   ReactGA.initialize('UA-48705802-13', {
     // Uncomment line below to get details in the console when developing
-    debug: isDevelopment,
+    debug: isDevelopment && isGADebug,
     gaOptions: {
       siteSpeedSampleRate: 100
     }
