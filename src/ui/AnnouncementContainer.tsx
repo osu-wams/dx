d4cfcc1614abd6212047b68e6e32758d6567a9bc
Nick Piatt <developer.piatt@gmail.com>
2019-10-18 15:08:34 -0700

    removed unused import

--- src/ui/AnnouncementContainer.tsx
+++ src/ui/AnnouncementContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { useAnnouncements } from '../api/announcements';
 import EventCard from './EventCard';
