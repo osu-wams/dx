ec5613687a6b33e0c179681119f8db0b087bd4b3
Nick Piatt <developer.piatt@gmail.com>
2019-10-28 10:20:42 -0700

    put in some logic to hide the announcements when no announcements are present

--- src/pages/Academics/AcademicsDashboard.tsx
+++ src/pages/Academics/AcademicsDashboard.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { faGraduationCap } from '@fortawesome/pro-light-svg-icons';
+
+import { UserContext } from '../../App';
+import { useAnnouncements } from '../../api/announcements';
+import { hasAudience } from '../../api/user';
+
 import Courses from '../../features/Courses';
 import AnnouncementContainer from '../../ui/AnnouncementContainer';
 import { MainGridWrapper, MainGrid, MainGridCol, SecondGridWrapper } from '../../ui/PageGrid';
@@ -11,6 +16,34 @@ import ResourcesCard from '../../features/ResourcesCard';
 import { AcademicSubNav } from './AcademicsSubNav';
 
 const AcademicsDashboard = () => {
+  /**
+   * The following code was lifted from the announcement container. I'm pretty sure this works, but it does
+   * seem very redundant to have this code in two places being called twice. but this is the only way I 
+   * knew how to get the events length, was to call it here. which means i probably could remove it from
+   * the other file
+   */
+
+  const [events, setEvents] = useState<any>([]);
+  const user = useContext<any>(UserContext);
+  const announcements = useAnnouncements('academic');
+  
+  /* eslint-disable react-hooks/exhaustive-deps */
+  // Fetch data on load
+  useEffect(() => {
+    let announcementsToUse: any[] = [];
+
+    if (!user.loading && !announcements.loading) {
+      announcementsToUse = announcements.data.filter(announcement =>
+        hasAudience(user.data, announcement)
+      );
+    }
+    setEvents(announcementsToUse);
+  }, [announcements.data, announcements.loading, user.data, user.loading]);
+  /* eslint-enable react-hooks/exhaustive-deps */
+
+  console.log(events.length)
+
+
   return (
     <>
       <MainGridWrapper>
@@ -29,8 +62,12 @@ const AcademicsDashboard = () => {
         </MainGrid>
       </MainGridWrapper>
       <SecondGridWrapper>
-        <Title as="h2">Announcements</Title>
-        <AnnouncementContainer className="col-span-2" type="academic" />
+        {events.length > 0 && (
+          <>
+          <Title as="h2">Announcements</Title>
+          <AnnouncementContainer className="col-span-2" type="academic" />
+          </>
+        )}
       </SecondGridWrapper>
     </>
   );
--- src/pages/Finances.tsx
+++ src/pages/Finances.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { faDollarSign } from '@fortawesome/pro-light-svg-icons';
+
+import { UserContext } from '../App';
+import { useAnnouncements } from '../api/announcements';
+import { hasAudience } from '../api/user';
+
 import PageTitle, { Title } from '../ui/PageTitle';
 import ResourcesCard from '../features/ResourcesCard';
 import AnnouncementContainer from '../ui/AnnouncementContainer';
@@ -8,6 +13,34 @@ import FinancialOverview from '../features/financial-overview/FinancialOverview'
 import { MainGridWrapper, MainGrid, MainGridCol, SecondGridWrapper } from '../ui/PageGrid';
 
 const Finances = () => {
+
+  /**
+   * The following code was lifted from the announcement container. I'm pretty sure this works, but it does
+   * seem very redundant to have this code in two places being called twice. but this is the only way I 
+   * knew how to get the events length, was to call it here. which means i probably could remove it from
+   * the other file
+   */
+
+  const [events, setEvents] = useState<any>([]);
+  const user = useContext<any>(UserContext);
+  const announcements = useAnnouncements('financial');
+  
+  /* eslint-disable react-hooks/exhaustive-deps */
+  // Fetch data on load
+  useEffect(() => {
+    let announcementsToUse: any[] = [];
+
+    if (!user.loading && !announcements.loading) {
+      announcementsToUse = announcements.data.filter(announcement =>
+        hasAudience(user.data, announcement)
+      );
+    }
+    setEvents(announcementsToUse);
+  }, [announcements.data, announcements.loading, user.data, user.loading]);
+  /* eslint-enable react-hooks/exhaustive-deps */
+
+  console.log(events.length)
+
   return (
     <div data-testid="finances-page">
       <MainGridWrapper>
@@ -23,8 +56,12 @@ const Finances = () => {
         </MainGrid>
       </MainGridWrapper>
       <SecondGridWrapper>
-        <Title as="h2">Announcements</Title>
-        <AnnouncementContainer className="col-span-2" type="financial" />
+      {events.length > 0 && (
+          <>
+          <Title as="h2">Announcements</Title>
+          <AnnouncementContainer className="col-span-2" type="financial" />
+          </>
+        )}
       </SecondGridWrapper>
     </div>
   );
