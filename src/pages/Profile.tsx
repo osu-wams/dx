0afbe5106eea6c3459c6036bb733650ee555c6b3
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2019-11-06 14:50:22 -0800

    Initial card setup for Dashboard settings

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import PageTitle from '../ui/PageTitle';
 import OSUProfile from '../features/profile/OSUProfile';
-import { MainGridWrapper, MainGrid } from '../ui/PageGrid';
+import Settings from '../features/profile/Settings';
+import { MainGridWrapper, MainGrid, MainGridCol } from '../ui/PageGrid';
 
 const Profile = () => (
   <div data-testid="profile-page">
     <MainGridWrapper>
       <PageTitle title="My Profile" />
       <MainGrid>
-        <OSUProfile />
+        <MainGridCol>
+          <OSUProfile />
+        </MainGridCol>
+        <MainGridCol>
+          <Settings />
+        </MainGridCol>
       </MainGrid>
     </MainGridWrapper>
   </div>
