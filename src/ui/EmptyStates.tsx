9dedab577e29d2fc6f24c717bf15b3b8f1d79f45
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2020-01-09 08:50:27 -0800

    New Empty State reusable component, adding it to Courses and Planner items

---
+++ src/ui/EmptyStates.tsx
@@ -0,0 +1,21 @@
+import { styled, themeSettings } from '../theme';
+
+const EmptyState = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: column;
+  align-items: center;
+  padding: ${themeSettings.spacing.unit * 4}px ${themeSettings.spacing.unit * 8}px 0px
+    ${themeSettings.spacing.unit * 8}px;
+`;
+
+const EmptyStateImage = styled.img`
+  height: 70px;
+`;
+
+const EmptyStateText = styled.p`
+  color: ${({ theme }) => theme.features.academics.courses.plannerItems.emptyText.color};
+  text-align: center;
+`;
+
+export { EmptyState, EmptyStateImage, EmptyStateText };
