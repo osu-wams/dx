228c0e4979ea282c781fabc1e1f74ab37192d76d
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2020-01-13 09:26:36 -0800

    Adds beta dashboard pageContent tests

---
+++ src/api/__mocks__/pageContent.data.ts
@@ -0,0 +1,8 @@
+export default {
+  data: [
+    {
+      title: 'Beta Page Title',
+      content: `<p>This is the beta release of an all-new student portal, intended to (eventually!)</p><ul><li>beta list item</li></ul><p>This is a <a href="https://orst.edu">beta link</a></p>`
+    }
+  ]
+};
--- src/features/__tests__/BetaDashboard.test.tsx
+++ src/features/__tests__/BetaDashboard.test.tsx
@@ -3,12 +3,41 @@ import { fireEvent, waitForElement } from '@testing-library/react';
 import { render } from '../../util/test-utils';
 import BetaDashboard from '../../pages/BetaDashboard';
 import { mockGAEvent } from '../../setupTests';
+import betaContentMock from '../../api/__mocks__/pageContent.data';
+import betaReleaseNotes from '../../api/__mocks__/releaseNotes.data';
 
+const mockUsePageContent = jest.fn();
+const mockUseReleaseNotes = jest.fn();
+
+jest.mock('../../api/page-content', () => ({
+  usePageContent: () => mockUsePageContent()
+}));
+
+jest.mock('../../api/release-notes', () => ({
+  useReleaseNotes: () => mockUseReleaseNotes()
+}));
+
+beforeEach(() => {
+  mockUsePageContent.mockReturnValue(betaContentMock);
+  mockUseReleaseNotes.mockReturnValue(betaReleaseNotes);
+});
 
 describe('<BetaDashboard />', () => {
   it('should render the beta dashboard page', async () => {
     const { getByTestId } = render(<BetaDashboard />);
     expect(getByTestId('betadash-page')).toBeInTheDocument();
+  });
+
+  it('should display the title Beta', async () => {
+    const { getByText } = render(<BetaDashboard />);
+    await waitForElement(() => getByText('Beta', { selector: 'h1' }));
+  });
+
+  it('should display "Dashboard Beta" card title with appropriate content', async () => {
+    const { findByText } = render(<BetaDashboard />);
+    expect(await findByText(/Dashboard Beta/i, { selector: 'h2 span' })).toBeInTheDocument();
+    expect(await findByText('Beta Page Title')).toBeInTheDocument();
+    expect(await findByText('beta list item', { selector: 'li' })).toBeInTheDocument();
   });
 
   it('should have links that are tracked via GA', async () => {
@@ -20,10 +49,5 @@ describe('<BetaDashboard />', () => {
     fireEvent.click(getHelp);
     fireEvent.click(giveFeedback);
     expect(mockGAEvent).toHaveBeenCalledTimes(3);
-  });
-
-  it('should display the title Beta', async () => {
-    const { getByText } = render(<BetaDashboard />);
-    await waitForElement(() => getByText('Beta'));
   });
 });
