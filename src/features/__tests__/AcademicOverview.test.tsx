c7e329315a5f9693fa41a318a3c9e80da0b9de21
Josh Gum <revgum@gmail.com>
2020-01-28 12:29:03 -0800

    Grad student academic dashboard (#244)
    * adds latest @osu-wams dependencies
    
    * filters data from graduate students
    
    * use types import from @osu-wams/lib
    
    * updates @osu/wams packages to include latest functionality
    
    * fixes test for setting a non-grad student

--- src/features/__tests__/AcademicOverview.test.tsx
+++ src/features/__tests__/AcademicOverview.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { waitForElement, fireEvent } from '@testing-library/react';
-import { render } from '../../util/test-utils';
+import { render, mockUser, authUser } from '../../util/test-utils';
 import AcademicOverview from '../AcademicOverview';
 import { mockGAEvent } from '../../setupTests';
 import { Student } from '@osu-wams/hooks';
@@ -25,12 +25,27 @@ jest.mock('@osu-wams/hooks', () => {
 });
 
 describe('<Academic Overview />', () => {
+  it('Academic Overview will filter some content out for graduate students ', async () => {
+    mockUseStudentGpa.mockReturnValue(gpaHookData);
+    mockUseAcademicStatus.mockReturnValue(mockAcademicStatus);
+    mockUseCourseSchedule.mockReturnValue(mockCourseSchedule);
+    mockUseHolds.mockReturnValue(mockHolds);
+    const { queryByText } = render(<AcademicOverview />);
+    const link = queryByText('View more in MyDegrees');
+    expect(link).not.toBeInTheDocument();
+    const academicStanding = queryByText('Academic Standing');
+    expect(academicStanding).not.toBeInTheDocument();
+  });
   it('Academic Overview has a footer that can be clicked to access My Degrees', async () => {
     mockUseStudentGpa.mockReturnValue({ ...gpaHookData, data: gpaUndergraduateData });
     mockUseAcademicStatus.mockReturnValue(mockAcademicStatus);
     mockUseCourseSchedule.mockReturnValue(mockCourseSchedule);
     mockUseHolds.mockReturnValue(mockHolds);
-    const { getByText } = render(<AcademicOverview />);
+    const mockUser = authUser;
+    mockUser.data.classification!.attributes!.levelCode = '';
+    mockUser.data.audienceOverride = {};
+
+    const { getByText } = render(<AcademicOverview />, { user: mockUser });
     const element = await waitForElement(() => getByText('View more in MyDegrees'));
     fireEvent.click(element);
     expect(mockGAEvent).toHaveBeenCalled();
