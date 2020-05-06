417b926f5e3b0ae3b7b473cfa38d06ca8f55de39
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2020-05-06 08:26:41 -0700

    updates favorite resources and financial overview

--- src/features/__tests__/FinancialOverview.test.tsx
+++ src/features/__tests__/FinancialOverview.test.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { render } from 'src/util/test-utils';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import FinancialOverview from '../financial-overview/FinancialOverview';
 import { mockGAEvent } from 'src/setupTests';
 
 it('should find link to view and make payment and clicking it triggers analytics', async () => {
-  const { getByText, findByText } = render(<FinancialOverview />);
-  await findByText('Financial Overview');
-  const MakePayment = getByText('Make a payment');
-  const AddMoney = getByText('Add money');
+  render(<FinancialOverview />);
+  await screen.findByText('Financial Overview');
+  const MakePayment = screen.getByText('Make a payment');
+  const AddMoney = screen.getByText('Add money');
 
   userEvent.click(MakePayment);
   userEvent.click(AddMoney);
