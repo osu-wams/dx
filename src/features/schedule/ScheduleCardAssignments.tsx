f8332b7b956b7f161184d014a168a9d84a969fb3
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2020-05-14 12:52:53 -0700

    modifies ScheduleCard Assignments to reuse the new CanvasPlannerItems component

--- src/features/schedule/ScheduleCardAssignments.tsx
+++ src/features/schedule/ScheduleCardAssignments.tsx
@@ -1,6 +1,4 @@
 import React, { useContext } from 'react';
-import { ThemeContext } from 'styled-components/macro';
-import { faFileAlt } from '@fortawesome/pro-light-svg-icons';
 import {
   CardSection,
   SectionHeader,
@@ -8,25 +6,20 @@ import {
   NoItemsImage,
   NoItemsText,
 } from './ScheduleCardStyles';
-import Url from 'src/util/externalUrls.data';
-import Icon from 'src/ui/Icon';
 import assignment from 'src/assets/assignment.svg';
-import {
-  List,
-  ListItem,
-  ListItemHeader,
-  ListItemDescription,
-  ListItemText,
-  ListItemContentLink,
-} from 'src/ui/List';
+import { List } from 'src/ui/List';
 import { AuthorizeCanvasCompact } from '../canvas/AuthorizeCanvas';
-import { Event } from 'src/util/gaTracking';
-import { courseCodeOrIcon } from '../Courses';
-import { format } from 'src/util/helpers';
 import { AppContext } from 'src/contexts/app-context';
+import { CanvasPlannerItems } from 'src/features/canvas/CanvasPlannerItems';
+import { Types } from '@osu-wams/lib';
 
-const ScheduleCardAssignments = ({ selectedPlannerItems, courseList }) => {
-  const themeContext = useContext(ThemeContext);
+const ScheduleCardAssignments = ({
+  selectedPlannerItems,
+  courseList,
+}: {
+  selectedPlannerItems: Types.PlannerItem[];
+  courseList: Types.CourseSchedule[];
+}) => {
   const { user } = useContext(AppContext);
 
   const noAssignmentsDue = () => (
@@ -42,45 +35,9 @@ const ScheduleCardAssignments = ({ selectedPlannerItems, courseList }) => {
       <List>
         {!user.isCanvasOptIn && <AuthorizeCanvasCompact />}
         {user.isCanvasOptIn && selectedPlannerItems.length === 0 && noAssignmentsDue()}
-        {user.isCanvasOptIn &&
-          selectedPlannerItems.length > 0 &&
-          selectedPlannerItems.map(
-            ({
-              context_name,
-              plannable_id,
-              html_url,
-              plannable_type,
-              plannable_date,
-              plannable: { title },
-            }) => (
-              <ListItem key={plannable_id}>
-                <ListItemContentLink
-                  href={Url.canvas.main + html_url ?? ''}
-                  onClick={() =>
-                    Event('schedule-card', 'canvas-link', `${Url.canvas.main + html_url ?? title}`)
-                  }
-                >
-                  {courseCodeOrIcon(
-                    context_name,
-                    courseList,
-                    <Icon
-                      icon={faFileAlt}
-                      color={themeContext.features.academics.courses.plannerItems.list.icon.color}
-                    />
-                  )}
-                  <ListItemText>
-                    <ListItemHeader>{title} </ListItemHeader>
-                    <ListItemDescription>
-                      {plannable_type &&
-                        plannable_date &&
-                        plannable_type !== 'announcement' &&
-                        `Due ${format(plannable_date, 'dueAt')}`}
-                    </ListItemDescription>
-                  </ListItemText>
-                </ListItemContentLink>
-              </ListItem>
-            )
-          )}
+        {user.isCanvasOptIn && selectedPlannerItems.length > 0 && (
+          <CanvasPlannerItems data={selectedPlannerItems} courses={courseList} />
+        )}
       </List>
     </CardSection>
   );
