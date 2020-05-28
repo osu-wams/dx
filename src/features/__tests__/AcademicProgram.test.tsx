1f653e754568133ef880dd6d8b8b5a210f215be2
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2020-05-28 08:00:44 -0700

    updates Empcenter description, tests now follow newer convention of using screen.* for AcademicProgram and EmployeeTools

--- src/features/__tests__/AcademicProgram.test.tsx
+++ src/features/__tests__/AcademicProgram.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render } from 'src/util/test-utils';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { render } from 'src/util/test-utils';
 import { AcademicProgram } from '../AcademicProgram';
 import { Student } from '@osu-wams/hooks';
 import { mockGAEvent } from 'src/setupTests';
@@ -21,42 +22,42 @@ beforeEach(() => {
 
 describe('<ProgramOfStudy /> | Degree', () => {
   it('Expects "Program of Study" card to render', () => {
-    const { getByText } = render(<AcademicProgram />);
+    render(<AcademicProgram />);
 
-    expect(getByText(/My Academic Program/i)).toBeInTheDocument();
+    expect(screen.getByText(/My Academic Program/i)).toBeInTheDocument();
   });
 
   it('Expects "corvallis" to show up as the Campus', () => {
-    const { getByText } = render(<AcademicProgram />);
+    render(<AcademicProgram />);
 
-    expect(getByText(/corvallis/i)).toBeInTheDocument();
+    expect(screen.getByText(/corvallis/i)).toBeInTheDocument();
   });
 
   it('Expects Degree data to show up', () => {
-    const { getByText } = render(<AcademicProgram />);
+    render(<AcademicProgram />);
 
-    expect(getByText(/Bachelor of Science/i)).toBeInTheDocument();
-    expect(getByText(/College of Engineering/i)).toBeInTheDocument();
+    expect(screen.getByText(/Bachelor of Science/i)).toBeInTheDocument();
+    expect(screen.getByText(/College of Engineering/i)).toBeInTheDocument();
   });
 
   it('Expects 2 majors to show up along the same department each time', () => {
-    const { getByText, getAllByText } = render(<AcademicProgram />);
+    render(<AcademicProgram />);
 
-    expect(getByText(/Mechanical Engineering/i)).toBeInTheDocument();
-    expect(getByText(/Manufacturing Engineering/i)).toBeInTheDocument();
-    expect(getAllByText(/School of Mech, Ind, Manf Engr/i)).toHaveLength(2);
+    expect(screen.getByText(/Mechanical Engineering/i)).toBeInTheDocument();
+    expect(screen.getByText(/Manufacturing Engineering/i)).toBeInTheDocument();
+    expect(screen.getAllByText(/School of Mech, Ind, Manf Engr/i)).toHaveLength(2);
   });
 
   it('Expects 2 minors to show up', () => {
-    const { getByText } = render(<AcademicProgram />);
+    render(<AcademicProgram />);
 
-    expect(getByText(/Spanish/i)).toBeInTheDocument();
-    expect(getByText(/Education/i)).toBeInTheDocument();
+    expect(screen.getByText(/Spanish/i)).toBeInTheDocument();
+    expect(screen.getByText(/Education/i)).toBeInTheDocument();
   });
 
   it('Expects Student Profile Link tracked by Analytics', () => {
-    const { getByText } = render(<AcademicProgram />);
-    const profileLink = getByText(/Student Profile/i);
+    render(<AcademicProgram />);
+    const profileLink = screen.getByText(/Student Profile/i);
     expect(profileLink).toBeInTheDocument();
 
     userEvent.click(profileLink);
@@ -65,9 +66,9 @@ describe('<ProgramOfStudy /> | Degree', () => {
 
   it('Expects "No Information", not "Bachelor of Science" when no data returns', () => {
     mockUseDegrees.mockReturnValue({ data: [] });
-    const { getByText, queryByText } = render(<AcademicProgram />);
+    render(<AcademicProgram />);
 
-    expect(queryByText(/Bachelor of Science/i)).toBeNull();
-    expect(getByText(/you do not currently have a program of study/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Bachelor of Science/i)).toBeNull();
+    expect(screen.getByText(/you do not currently have a program of study/i)).toBeInTheDocument();
   });
 });
