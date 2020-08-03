7af59be4baef25c73c4b6a15fecd5290202f9b9c
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2020-08-03 13:31:02 -0700

    Adds constant for API paths

---
+++ src/mocks/apis.ts
@@ -0,0 +1,27 @@
+/**
+ * API Endpoints
+ */
+
+const stu = '/api/student/';
+const events = '/api/events/';
+const an = '/api/announcements/';
+/**
+ * Student API paths
+ */
+export const HOLDS_API = stu + 'holds';
+
+export const GPA_API = stu + 'gpa';
+
+export const ACADEMIC_STATUS_API = stu + 'academic-status';
+
+export const CLASS_SCHEDULE_API = stu + 'class-schedule?term=current';
+
+/**
+ * Events API
+ */
+export const ACADEMIC_CALENDAR_API = events + 'academic-calendar';
+
+/**
+ * Anouncements
+ */
+export const ACADEMIC_ANNOUNCEMENTS_API = events + '*';
