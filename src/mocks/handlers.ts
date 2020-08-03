09486db2eb2eacb73b40394456aad2f6e0fec3da
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2020-08-03 09:01:06 -0700

    Adds msw to AcademicOverview and mocks more endpoints

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,27 @@
 import { rest } from 'msw';
 import { Student } from '@osu-wams/hooks';
 
-const mockHolds = Student.Holds.mockHolds;
+const mockHolds = Student.Holds.mockHolds.data;
+const mockAcademicStatus = Student.AcademicStatus.mockAcademicStatus.data;
+const mockCourseSchedule = Student.CourseSchedule.mockCourseSchedule.schedule.data;
+const { gpaHookData, gpaUndergraduateData } = Student.Gpa.mockGpa;
+const mockGpa = { ...gpaHookData, data: gpaUndergraduateData };
 
 // Mock API Data for our Endpoints
 export const handlers = [
   rest.get('/api/student/holds', async (req, res, ctx) => {
-    return res(ctx.json(mockHolds.data));
+    return res(ctx.json(mockHolds));
+  }),
+
+  rest.get('/api/student/gpa', async (req, res, ctx) => {
+    return res(ctx.json(mockGpa.data));
+  }),
+
+  rest.get('/api/student/academic-status', async (req, res, ctx) => {
+    return res(ctx.json(mockAcademicStatus));
+  }),
+
+  rest.get('/api/student/class-schedule?term=current', async (req, res, ctx) => {
+    return res(ctx.json(mockCourseSchedule));
   }),
 ];
