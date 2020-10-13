46a6b67ec469b98ee3cbcbf9c01fb8889fd66fe6
Josh Gum <revgum@gmail.com>
2020-10-13 08:58:08 -0700

    adds utility hook for setting a page scroll to top

---
+++ src/util/useResetScroll.ts
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useLocation, useNavigate } from '@reach/router';
+
+export const useResetScroll = () => {
+  const navigate = useNavigate();
+  const { href, state } = useLocation();
+
+  const updateState = React.useCallback(() => {
+    navigate(href, {
+      // @ts-ignore spread object
+      state: { ...state, scrolled: true },
+      replace: true,
+    }).then(() => window.scrollTo(0, 0));
+  }, [href, state, navigate]);
+
+  React.useLayoutEffect(() => {
+    // @ts-ignore type unknown
+    if (!state?.scrolled) {
+      updateState();
+    }
+  }, [state, updateState]);
+};
