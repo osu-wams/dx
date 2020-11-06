ccc4341613aab091877211c0b23b3f677d35edd0
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2020-11-05 16:50:05 -0800

    Updates Info and ReleaseNotes to use react-query

--- src/features/about/Info.tsx
+++ src/features/about/Info.tsx
@@ -2,18 +2,20 @@ import React, { FC } from 'react';
 import { Card, CardContent, CardFooter } from 'src/ui/Card';
 import { usePageContent } from '@osu-wams/hooks';
 import { RichTextContent, RichTextTitle } from 'src/ui/RichText';
+import { Loading } from 'src/ui/Loading';
 
 const Info: FC = () => {
-  const pageContent = usePageContent('about');
+  const { isSuccess, isLoading, data } = usePageContent('about');
 
   return (
     <Card collapsing={false}>
-      <CardContent>
-        <RichTextTitle>{pageContent.data[0]?.title}</RichTextTitle>
-        <RichTextContent
-          dangerouslySetInnerHTML={{ __html: pageContent.data[0]?.content }}
-        ></RichTextContent>
-      </CardContent>
+      {isLoading && <Loading lines={4} />}
+      {isSuccess && data && (
+        <CardContent>
+          <RichTextTitle>{data[0]?.title}</RichTextTitle>
+          <RichTextContent dangerouslySetInnerHTML={{ __html: data[0]?.content }}></RichTextContent>
+        </CardContent>
+      )}
       <CardFooter></CardFooter>
     </Card>
   );
--- src/features/about/ReleaseNotes.tsx
+++ src/features/about/ReleaseNotes.tsx
@@ -3,14 +3,19 @@ import { faClipboardListCheck } from '@fortawesome/pro-light-svg-icons';
 import { Card, CardHeader, CardContent, CardIcon, CardFooter } from 'src/ui/Card';
 import { useReleaseNotes } from '@osu-wams/hooks';
 import { RichTextContent, RichTextTitle } from 'src/ui/RichText';
+import { Loading } from 'src/ui/Loading';
 
 const ReleaseNotes: FC = () => {
-  const releaseNotes = useReleaseNotes();
+  const { data, isLoading, isSuccess } = useReleaseNotes();
 
   return (
     <>
-      {releaseNotes.data.length &&
-        releaseNotes.data.slice(0, 3).map((releaseNote, i) => (
+      {isLoading && <Loading lines={5} />}
+
+      {isSuccess &&
+        data &&
+        data.length &&
+        data.slice(0, 3).map((releaseNote, i) => (
           <Card key={`release-notes-${i}`}>
             <CardHeader
               title={`Release : ${releaseNote?.date}`}
