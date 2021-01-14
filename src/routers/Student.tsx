9115297eff2f9fc04b44d5d70ce03f4dbccaea15
Josh Gum <revgum@gmail.com>
2021-01-13 17:53:10 -0800

    reduce routes to only components relative to that type of router

--- src/routers/Student.tsx
+++ src/routers/Student.tsx
@@ -1,32 +1,24 @@
 import { Router } from '@reach/router';
 import React from 'react';
-import About from '../pages/About';
 import Dashboard from '../pages/Dashboard';
-import MobileCovid from '../pages/mobile-app/MobileCovid';
-import Notifications from '../pages/Notifications';
-import PageNotFound from '../pages/PageNotFound';
 import Profile from '../pages/Profile';
 import Academics from '../pages/Academics';
 import Finances from '../pages/Finances';
 import Resources from '../pages/Resources';
 import { useResetScroll } from 'src/util/useResetScroll';
 import { RouterPage } from '.';
+import PageNotFound from 'src/pages/PageNotFound';
 
 export const Student = () => {
   useResetScroll();
   return (
     <Router basepath="/student" key="student-dashboard" className="router-styles">
+      <RouterPage default pageComponent={<PageNotFound />} />
       <RouterPage path="/" pageComponent={<Dashboard />} />
       <RouterPage path="profile" pageComponent={<Profile />} />
       <RouterPage path="academics/*" pageComponent={<Academics />} />
       <RouterPage path="finances" pageComponent={<Finances />} />
       <RouterPage path="resources" pageComponent={<Resources />} />
-      <RouterPage path="about" pageComponent={<About />} />
-      <RouterPage path="notifications" pageComponent={<Notifications />} />
-      <RouterPage default pageComponent={<PageNotFound />} />
-      {process.env.REACT_APP_EXPERIMENTAL === 'true' && (
-        <RouterPage path="covid" pageComponent={<MobileCovid />} />
-      )}
     </Router>
   );
 };
