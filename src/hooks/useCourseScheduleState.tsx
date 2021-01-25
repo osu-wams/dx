39da1759b8489e08c3400cd3770077f86a98bc46
Josh Gum <revgum@gmail.com>
2021-01-25 11:48:12 -0800

    wraps osu-wams hook in a local state hook

---
+++ src/hooks/useCourseScheduleState.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useCourseSchedule } from '@osu-wams/hooks';
+import { useRecoilState } from 'recoil';
+import { courseState } from 'src/state/courses';
+
+export const useCourseScheduleState = () => {
+  const api = useCourseSchedule();
+  const [courses, setCourses] = useRecoilState(courseState);
+
+  useEffect(() => {
+    const { isError, isLoading, isSuccess, data } = api;
+    // Only reset application state when the api has returned new data that isn't already set
+    if (isSuccess && data && data !== courses.data) {
+      setCourses({ data, isLoading, isSuccess, isError });
+    }
+  }, [api.data, api.isSuccess]);
+
+  return { courses, setCourses };
+};
+
+export default useCourseScheduleState;
