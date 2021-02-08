622111c961f847310655bb03edc72b8e532cdea2
Josh Gum <revgum@gmail.com>
2021-02-08 06:36:01 -0800

    loads gcse async in component

--- src/features/application-search/GoogleSearchResults.tsx
+++ src/features/application-search/GoogleSearchResults.tsx
@@ -1,23 +1,47 @@
 import React, { useEffect, useRef } from 'react';
-import { useRecoilValue } from 'recoil';
-import { debouncedApplicationSearchState } from 'src/state/search';
+import { Helmet } from 'react-helmet-async';
+import { useRecoilState } from 'recoil';
+import { applicationSearchState } from 'src/state/applicationSearch';
+import { useLocation } from '@reach/router';
 
 const GoogleSearchResults: React.FC<any> = () => {
+  const location = useLocation();
   const gcseResults = useRef<HTMLDivElement>(null);
-  const debouncedQuery = useRecoilValue(debouncedApplicationSearchState);
+  const [query, setQuery] = useRecoilState(applicationSearchState);
+
+  // If navigating to the page programatically, check location state (window.history)
+  // for a query to initialize the page with after Google Custom Search is initialized
+  useEffect(() => {
+    // @ts-ignore Reach Router location state provided during navigate
+    const locationQuery = location.state.query;
+    if (locationQuery) {
+      // Shameless hack way to cause Google Custom Search (GCSE) to execute after first render when
+      // gcse-results div isn't ready to execute for search results.
+      // I can't find a better way (tried Googles recommended callback) as well as a few others.
+      // https://developers.google.com/custom-search/docs/element#callbacks
+      setTimeout(() => setQuery(locationQuery), 10);
+    }
+  }, []);
 
   useEffect(() => {
-    if (debouncedQuery) {
-      // @ts-ignore
-      const gcseElement = window.google?.search?.cse?.element;
+    if (query) {
+      // @ts-ignore window.google exists after async script in Helmet is loaded
+      const element = window.google?.search?.cse?.element;
+      const gcseElement = element?.getElement('gcse-results');
       if (gcseElement) {
-        gcseElement.getElement('gcse-results').execute(debouncedQuery);
+        gcseElement.execute(query);
       }
     }
-  }, [debouncedQuery]);
+  }, [query]);
 
   return (
-    <div style={{ display: debouncedQuery ? 'block' : 'none' }}>
+    <div style={{ display: query ? 'block' : 'none' }}>
+      <Helmet>
+        <script
+          async
+          src="https://cse.google.com/cse.js?cx=001157565620839607635:f_5ovr-jasm"
+        ></script>
+      </Helmet>
       <div
         ref={gcseResults}
         className="gcse-searchresults-only"
