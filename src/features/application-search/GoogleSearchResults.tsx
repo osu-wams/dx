b7fb8564124aed65b082b96ae12906fe4f31f6da
Josh Gum <revgum@gmail.com>
2021-02-08 10:03:09 -0800

    executes gcse searches when search term is updated

--- src/features/application-search/GoogleSearchResults.tsx
+++ src/features/application-search/GoogleSearchResults.tsx
@@ -1,41 +1,25 @@
 import React, { useEffect, useRef } from 'react';
 import { Helmet } from 'react-helmet-async';
-import { useRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { applicationSearchState } from 'src/state/applicationSearch';
-import { useLocation } from '@reach/router';
 
 const GoogleSearchResults: React.FC<any> = () => {
-  const location = useLocation();
   const gcseResults = useRef<HTMLDivElement>(null);
-  const [query, setQuery] = useRecoilState(applicationSearchState);
-
-  // If navigating to the page programatically, check location state (window.history)
-  // for a query to initialize the page with after Google Custom Search is initialized
-  useEffect(() => {
-    // @ts-ignore Reach Router location state provided during navigate
-    const locationQuery = location.state.query;
-    if (locationQuery) {
-      // Shameless hack way to cause Google Custom Search (GCSE) to execute after first render when
-      // gcse-results div isn't ready to execute for search results.
-      // I can't find a better way (tried Googles recommended callback) as well as a few others.
-      // https://developers.google.com/custom-search/docs/element#callbacks
-      setTimeout(() => setQuery(locationQuery), 10);
-    }
-  }, []);
+  const search = useRecoilValue(applicationSearchState);
 
   useEffect(() => {
-    if (query) {
+    if (search) {
       // @ts-ignore window.google exists after async script in Helmet is loaded
       const element = window.google?.search?.cse?.element;
       const gcseElement = element?.getElement('gcse-results');
       if (gcseElement) {
-        gcseElement.execute(query);
+        gcseElement.execute(search);
       }
     }
-  }, [query]);
+  }, [search]);
 
   return (
-    <div style={{ display: query ? 'block' : 'none' }}>
+    <div style={{ display: search ? 'block' : 'none' }}>
       <Helmet>
         <script
           async
