3460595b411f4a4fcbbee415c2ff5277a0eb6ed0
Josh Gum <revgum@gmail.com>
2021-03-11 09:26:35 -0800

    adds coverage for bubble ui

---
+++ src/ui/__tests__/Bubble.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from 'src/util/test-utils';
+import { Bubble, BubbleExternalLink, BubbleInternalLink } from '../Bubble';
+
+it('Renders default bubble', () => {
+  const { container } = render(<Bubble>Bubble text here</Bubble>);
+  expect(container).toMatchSnapshot();
+});
+
+it('Renders internal link bubble', () => {
+  const { container } = render(
+    <BubbleInternalLink>Bubble internal link text here</BubbleInternalLink>
+  );
+  expect(container).toMatchSnapshot();
+});
+
+it('Renders external link bubble', () => {
+  const { container } = render(
+    <BubbleExternalLink>Bubble external link text here</BubbleExternalLink>
+  );
+  expect(container).toMatchSnapshot();
+});
