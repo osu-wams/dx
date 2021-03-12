54a51da54556960bb28528bf357e677a1e31d8f4
Josh Gum <revgum@gmail.com>
2021-03-12 09:57:21 -0800

    adds coverage for search result list item UI

---
+++ src/ui/__tests__/SearchResultListItem.test.tsx
@@ -0,0 +1,144 @@
+import React from 'react';
+import Fuse from 'fuse.js';
+import { SearchItem } from 'src/state/search';
+import { render } from 'src/util/test-utils';
+import SearchResultListItem from '../ApplicationSearch/SearchResultListItem';
+import { Resources, Trainings, Student, User } from '@osu-wams/hooks';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+const { resourcesData } = Resources.mockResources;
+
+const training = Trainings.mockTrainings.data[0];
+const resource = resourcesData.data[0];
+const course = Student.CourseSchedule.mockCourseSchedule.courseScheduleHookData.data[0];
+const notification = User.mockUser.userMessage;
+
+describe('with a resource (typical) search result item', () => {
+  const { id, title, audiences, locations, link } = resource;
+  const resourceResult: Fuse.FuseResult<SearchItem> = {
+    item: {
+      attr: { resource },
+      id,
+      title,
+      type: 'Resource',
+      audience: audiences,
+      campuses: locations,
+      link: { href: link },
+      subText: { value: 'Some subtext here' },
+    },
+    refIndex: 1,
+  };
+  it('renders with an external link title', async () => {
+    render(<SearchResultListItem searchResult={resourceResult} />);
+    const link = await screen.findByText('Bend Testo Success Center', { selector: 'a' });
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute('href', 'https://success.oregonstate.edu/');
+  });
+  it('renders with an external link bubble with icon', async () => {
+    render(<SearchResultListItem searchResult={resourceResult} />);
+    const bubble = await screen.findByText('Resource', { selector: 'span' });
+    expect(bubble).toBeInTheDocument();
+    expect(bubble.lastChild).toMatchSnapshot();
+  });
+  it('renders with a simple subtext', async () => {
+    render(<SearchResultListItem searchResult={resourceResult} />);
+    const subtext = await screen.findByText('Some subtext here', { selector: 'div' });
+    expect(subtext).toBeInTheDocument();
+  });
+
+  it('renders without a modal', async () => {
+    render(<SearchResultListItem searchResult={resourceResult} />);
+    const link = await screen.findByText('Bend Testo Success Center', { selector: 'a' });
+    userEvent.click(link);
+    expect(screen.queryByText('Close', { selector: 'span' })).not.toBeInTheDocument();
+  });
+});
+
+describe('with a training search result item', () => {
+  const { id, title } = training;
+  const trainingResult: Fuse.FuseResult<SearchItem> = {
+    item: {
+      attr: { training },
+      id,
+      title,
+      type: 'Training',
+      audience: ['employee'],
+      link: { modal: true },
+      subText: { html: ['tag 1', 'tag 2'].join(' &bull; ') },
+    },
+    refIndex: 1,
+  };
+
+  it('renders with an internal link title', async () => {
+    render(<SearchResultListItem searchResult={trainingResult} />);
+    const link = await screen.findByText('Play nice with others', { selector: 'a' });
+    expect(link).toBeInTheDocument();
+    expect(link).not.toHaveAttribute('href');
+  });
+  it('renders with html subtext', async () => {
+    render(<SearchResultListItem searchResult={trainingResult} />);
+    const subtext = await screen.findByText(/tag 1 .* tag 2/, { selector: 'div' });
+    expect(subtext).toBeInTheDocument();
+  });
+  it('toggles the modal', async () => {
+    render(<SearchResultListItem searchResult={trainingResult} />);
+    const link = await screen.findByText('Play nice with others', { selector: 'a' });
+    userEvent.click(link);
+    expect(await screen.findByTestId('training-modal')).toBeInTheDocument();
+    const close = await screen.findByText('Close', { selector: 'span' });
+    userEvent.click(close);
+    expect(screen.queryByTestId('training-modal')).not.toBeInTheDocument();
+  });
+});
+
+describe('with a course search result item', () => {
+  const {
+    id,
+    attributes: { courseSubjectNumber, courseTitle },
+  } = course;
+  const courseResult: Fuse.FuseResult<SearchItem> = {
+    item: {
+      attr: { courses: { ...course } },
+      id,
+      title: courseSubjectNumber,
+      type: 'Current Course',
+      audience: ['employee'],
+      link: { modal: true },
+      subText: { value: courseTitle },
+    },
+    refIndex: 1,
+  };
+  it('toggles the modal', async () => {
+    render(<SearchResultListItem searchResult={courseResult} />);
+    const link = await screen.findByText('WR214', { selector: 'a' });
+    userEvent.click(link);
+    expect(await screen.findByTestId('course-dialog')).toBeInTheDocument();
+    const close = await screen.findByText('Close', { selector: 'span' });
+    userEvent.click(close);
+    expect(screen.queryByTestId('course-dialog')).not.toBeInTheDocument();
+  });
+});
+
+describe('with a notification search result item', () => {
+  const { messageId, title } = notification;
+  const notificationResult: Fuse.FuseResult<SearchItem> = {
+    item: {
+      attr: { notification },
+      id: messageId,
+      title,
+      type: 'Notification',
+      link: { modal: true },
+      subText: { value: 'Received at April 20, 2000' },
+    },
+    refIndex: 1,
+  };
+  it('toggles the modal', async () => {
+    render(<SearchResultListItem searchResult={notificationResult} />);
+    const link = await screen.findByText('Title', { selector: 'a' });
+    userEvent.click(link);
+    expect(await screen.findByTestId('notification-modal')).toBeInTheDocument();
+    const close = await screen.findByText('Close', { selector: 'span' });
+    userEvent.click(close);
+    expect(screen.queryByTestId('notification-modal')).not.toBeInTheDocument();
+  });
+});
