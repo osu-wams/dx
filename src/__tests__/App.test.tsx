b6e0db5101993af55774efc161b91045ba6890be
Josh Gum <revgum@gmail.com>
2021-03-17 10:46:18 -0700

    adds app level coverage for user hook and dashboard context changing

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { navigate } from '@reach/router';
+import { screen, waitFor } from '@testing-library/react';
+import { alterMock, render } from 'src/util/test-utils';
 import { LocationProvider, createHistory, createMemorySource } from '@reach/router';
 import App from '../App';
 import { RecoilRoot } from 'recoil';
+import { dashboardState, initialRouteState } from 'src/state/application';
+
+const mockPostSettings = jest.fn();
+jest.mock('@osu-wams/hooks', () => {
+  const original = jest.requireActual('@osu-wams/hooks');
+  return {
+    ...original,
+    User: {
+      ...original.User,
+      postSettings: (args) => mockPostSettings(args),
+    },
+  };
+});
+
+const mockNavigate = jest.fn();
+jest.mock('@reach/router', () => {
+  const original = jest.requireActual('@reach/router');
+  return {
+    ...original,
+    navigate: (route) => mockNavigate(route),
+  };
+});
 
 it('renders <App> without crashing', () => {
   const div = document.createElement('div');
@@ -17,4 +42,33 @@ it('renders <App> without crashing', () => {
     div
   );
   // ReactDOM.unmountComponentAtNode(div);
+});
+
+it('changes from student dashboard (default user) to employee dashboard', () => {
+  mockPostSettings.mockResolvedValue(true);
+  const div = document.createElement('div');
+  render(<App containerElement={div} />, {
+    initialStates: [
+      {
+        state: dashboardState,
+        value: { affiliation: 'employee', navigateTo: `/employee` },
+      },
+    ],
+  });
+  expect(mockPostSettings).toBeCalledTimes(1);
+  expect(mockPostSettings).toBeCalledWith({ primaryAffiliationOverride: 'employee' });
+});
+
+it('navigates to an initial route', async () => {
+  mockNavigate.mockResolvedValue(true);
+  const div = document.createElement('div');
+  render(<App containerElement={div} />, {
+    initialStates: [
+      {
+        state: initialRouteState,
+        value: '/about',
+      },
+    ],
+  });
+  expect(mockNavigate).toBeCalledWith('/about');
 });
