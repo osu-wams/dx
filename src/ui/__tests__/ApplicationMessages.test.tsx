aa40cf008d196755897d52c8015e61f6219a9a01
Josh Gum <revgum@gmail.com>
2021-03-18 08:21:23 -0700

    adds coverage for dismissing multiple messages

--- src/ui/__tests__/ApplicationMessages.test.tsx
+++ src/ui/__tests__/ApplicationMessages.test.tsx
@@ -172,3 +172,36 @@ it('close button dismisses a visible message', async () => {
   expect(screen.queryByText(title)).not.toBeInTheDocument();
   expect(screen.queryByText(body)).not.toBeInTheDocument();
 });
+
+it('close button dismisses each message on the stack one at a time', async () => {
+  const secondMockMessage = {
+    ...mockMessage,
+    id: 'second-mock-id',
+    title: 'Second mock title',
+    body: 'Second mock body',
+  };
+  render(<ApplicationMessages />, {
+    initialStates: [
+      {
+        state: messagesState,
+        value: [mockMessage, secondMockMessage],
+      },
+    ],
+  });
+
+  expect(screen.getByText(title)).toBeInTheDocument();
+  expect(screen.getByText(body)).toBeInTheDocument();
+
+  const dismissButton = screen.getByRole('button', { name: /close/i });
+  userEvent.click(dismissButton);
+  expect(screen.queryByText(title)).not.toBeInTheDocument();
+  expect(screen.queryByText(body)).not.toBeInTheDocument();
+
+  expect(screen.getByText(secondMockMessage.title)).toBeInTheDocument();
+  expect(screen.getByText(secondMockMessage.body)).toBeInTheDocument();
+
+  const secondDismissButton = screen.getByRole('button', { name: /close/i });
+  userEvent.click(secondDismissButton);
+  expect(screen.queryByText(secondMockMessage.title)).not.toBeInTheDocument();
+  expect(screen.queryByText(secondMockMessage.body)).not.toBeInTheDocument();
+});
