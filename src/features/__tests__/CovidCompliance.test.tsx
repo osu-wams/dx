1e7e8f555c4997c2be52128a2aa190c2cee4181f
Alex Ruef <alex.ruef@oregonstate.edu>
2021-10-07 15:28:29 -0700

    Updated CovidCompliance tests to work with grouper group

--- src/features/__tests__/CovidCompliance.test.tsx
+++ src/features/__tests__/CovidCompliance.test.tsx
@@ -1,28 +1,51 @@
 import React from 'react';
-import { renderWithAllContexts as render, mockEcampusUser } from 'src/util/test-utils';
+import { renderWithAllContexts as render } from 'src/util/test-utils';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Person } from '@osu-wams/hooks';
+import { Person, mockGrouper, State } from '@osu-wams/hooks';
 import CovidCompliance from '../CovidCompliance';
 import { mockGAEvent } from 'src/setupTests';
 
 const mockMedical = Person.Medical.mockMedical;
+const mockedGrouper = {
+  state: State.covidvacStudentState,
+  value: {
+    isLoading: false,
+    isSuccess: true,
+    data: mockGrouper,
+  },
+};
+const mockPerson = Person.Persons.mockPersons.personsData;
 const mockUseMedical = jest.fn();
+const mockUsePerson = jest.fn();
 
 jest.mock('@osu-wams/hooks', () => {
   return {
     ...jest.requireActual('@osu-wams/hooks'),
     useMedical: () => mockUseMedical(),
+    usePerson: () => mockUsePerson(),
   };
 });
 
 describe('<CovidCompliance />', () => {
   beforeEach(() => {
     mockUseMedical.mockReturnValue(mockMedical);
+    mockUsePerson.mockReturnValue(mockPerson);
   });
 
-  it('should not render for ecampus students', () => {
-    render(<CovidCompliance />, { user: mockEcampusUser });
+  it('should not render for users not in grouper group students', () => {
+    render(<CovidCompliance />, {
+      initialStates: [
+        {
+          state: State.covidvacStudentState,
+          value: {
+            isLoading: false,
+            isSuccess: true,
+            data: [],
+          },
+        },
+    ],
+  });
 
     expect(
       screen.queryByText(/covid vaccination/i, { selector: 'h2 > span' })
@@ -35,15 +58,13 @@ describe('<CovidCompliance />', () => {
     expect(screen.getByText(/covid vaccination/i, { selector: 'h2 > span' })).toBeInTheDocument();
   });
 
-  it('should have a positive vaccination message', () => {
-    render(<CovidCompliance />);
-
-    expect(screen.getByText(/you are in compliance/i)).toBeInTheDocument();
-  });
-
   it('should find links to become compliant', async () => {
     mockUseMedical.mockReturnValue({ ...mockMedical, data: [] });
-    render(<CovidCompliance />);
+    render(<CovidCompliance />, {
+      initialStates: [
+        mockedGrouper,
+      ],
+    });
     const getVaccinated = screen.getByText(/get the vaccination/i);
     const register = screen.getByText(/enter your vaccine information/i);
     const decline = screen.getByText(/complete an exemption form/i);
@@ -57,7 +78,11 @@ describe('<CovidCompliance />', () => {
 
   it('should show a negative vaccination message', async () => {
     mockUseMedical.mockReturnValue({ ...mockMedical, data: [] });
-    render(<CovidCompliance />);
+    render(<CovidCompliance />, {
+      initialStates: [
+        mockedGrouper,
+      ],
+    });
 
     expect(screen.getByText(/you are not in compliance/i)).toBeInTheDocument();
   });
