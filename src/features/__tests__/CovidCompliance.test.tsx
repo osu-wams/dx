27ae9713c50ba6d04ab298a13711c63349191d9d
Alex Ruef <alex.ruef@oregonstate.edu>
2021-10-08 15:25:51 -0700

    Fixed covidCompliance tests for useHasMember

--- src/features/__tests__/CovidCompliance.test.tsx
+++ src/features/__tests__/CovidCompliance.test.tsx
@@ -2,50 +2,22 @@ import React from 'react';
 import { renderWithAllContexts as render } from 'src/util/test-utils';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Person, mockGrouper, State } from '@osu-wams/hooks';
 import CovidCompliance from '../CovidCompliance';
 import { mockGAEvent } from 'src/setupTests';
 
-const mockMedical = Person.Medical.mockMedical;
-const mockedGrouper = {
-  state: State.covidvacStudentState,
-  value: {
-    isLoading: false,
-    isSuccess: true,
-    data: mockGrouper,
-  },
-};
-const mockPerson = Person.Persons.mockPersons.personsData;
-const mockUseMedical = jest.fn();
-const mockUsePerson = jest.fn();
+const mockUseHasMember = jest.fn();
 
 jest.mock('@osu-wams/hooks', () => {
   return {
     ...jest.requireActual('@osu-wams/hooks'),
-    useMedical: () => mockUseMedical(),
-    usePerson: () => mockUsePerson(),
+    useHasMember: () => mockUseHasMember(),
   };
 });
 
 describe('<CovidCompliance />', () => {
-  beforeEach(() => {
-    mockUseMedical.mockReturnValue(mockMedical);
-    mockUsePerson.mockReturnValue(mockPerson);
-  });
-
   it('should not render for users not in grouper group students', () => {
-    render(<CovidCompliance />, {
-      initialStates: [
-        {
-          state: State.covidvacStudentState,
-          value: {
-            isLoading: false,
-            isSuccess: true,
-            data: [],
-          },
-        },
-    ],
-  });
+    mockUseHasMember.mockReturnValue({ isLoading: false, isSuccess: true, data: false });
+    render(<CovidCompliance />);
 
     expect(
       screen.queryByText(/covid vaccination/i, { selector: 'h2 > span' })
@@ -53,18 +25,16 @@ describe('<CovidCompliance />', () => {
   });
 
   it('should have title: "Covid Vaccination"', () => {
+    mockUseHasMember.mockReturnValue({ isLoading: false, isSuccess: true, data: true });
     render(<CovidCompliance />);
 
     expect(screen.getByText(/covid vaccination/i, { selector: 'h2 > span' })).toBeInTheDocument();
   });
 
   it('should find links to become compliant', async () => {
-    mockUseMedical.mockReturnValue({ ...mockMedical, data: [] });
-    render(<CovidCompliance />, {
-      initialStates: [
-        mockedGrouper,
-      ],
-    });
+    mockUseHasMember.mockReturnValue({ isLoading: false, isSuccess: true, data: true });
+    render(<CovidCompliance />);
+
     const getVaccinated = screen.getByText(/get the vaccination/i);
     const register = screen.getByText(/enter your vaccine information/i);
     const decline = screen.getByText(/complete an exemption form/i);
@@ -77,12 +47,8 @@ describe('<CovidCompliance />', () => {
   });
 
   it('should show a negative vaccination message', async () => {
-    mockUseMedical.mockReturnValue({ ...mockMedical, data: [] });
-    render(<CovidCompliance />, {
-      initialStates: [
-        mockedGrouper,
-      ],
-    });
+    mockUseHasMember.mockReturnValue({ isLoading: false, isSuccess: true, data: true });
+    render(<CovidCompliance />);
 
     expect(screen.getByText(/you are not in compliance/i)).toBeInTheDocument();
   });
