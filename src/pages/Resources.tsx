c0dd7614ed4cbfdc8ca0a90df6c07f405630d06e
Alex Ruef <alex.ruef@oregonstate.edu>
2022-04-05 15:36:17 -0700

    remove category warn message

--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -61,7 +61,6 @@ const Resources = () => {
       category = decodeURI(HooksResources.defaultCategoryName());
     }
     if (typeof history.replaceState === 'function') {
-      console.warn(`Replacing history state: ${category}`);
       history.replaceState({ category }, category, `?category=${category}`);
     }
     setActiveCategory(category);
