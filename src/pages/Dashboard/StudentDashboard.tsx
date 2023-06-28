3a88962fa7179979a63aab1e3fdd077bee01dcd6
Alex Ruef <alex.ruef@oregonstate.edu>
2023-06-28 09:37:30 -0700

    removed extra empty line

--- src/pages/Dashboard/StudentDashboard.tsx
+++ src/pages/Dashboard/StudentDashboard.tsx
@@ -12,7 +12,6 @@ import { useRecoilValue } from 'recoil';
 import { DynamicCard } from 'src/ui/Card/variants/DynamicCard';
 import { ITSystemStatus } from 'src/features/it-systems-status/ITSystemStatus';
 import CovidCompliance from 'src/features/CovidCompliance';
-
 import BeaverhubAlert from 'src/features/BeaverhubAlert';
 
 const { ANNOUNCEMENT_PAGES, filteredCards } = State;
